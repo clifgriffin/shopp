8860ff3c1c92f782b340e13d4bf39c758061447c
Jonathan Davis <jond@ingenesis.net>
2008-08-21 06:59:49 +0000

    Fixed markup to be more flexible and fixed standards support Categories with no products will now be hidden completely
    git-svn-id: file:///Users/jond/svngit/svnrepos/shopp/trunk@120 846ab242-8549-0410-9a75-dfdbdae9473e

--- templates/category.php
+++ templates/category.php
@@ -1,23 +1,22 @@
-<div id="shopp">
-<?php shopp('catalog','breadcrumb'); ?>
-<h3><?php shopp('category','name'); ?></h3>
-
 <?php if(shopp('category','hasproducts')): ?>
 
-<ul class="products">
-	<li class="row"><ul>
-	<?php while(shopp('category','products')): ?>
-	<?php if(shopp('category','row','products=3')): ?></ul></li><li class="row"><ul><?php endif; ?>
-		<li class="product">
-			<div class="frame">
-			<?php shopp('category','product','thumbnail&link'); ?>
-			<h4 class="name"><?php shopp('category','product','name&link'); ?></h4>
-			<p class="price"><?php shopp('category','product','price'); ?></p>
-			</div>
-		</li>
-	<?php endwhile; ?>
-	</ul></li>
-</ul>
+	<?php shopp('catalog','breadcrumb'); ?>
+	<h3><?php shopp('category','name'); ?></h3>
 
+	<ul class="products">
+		<li class="row"><ul>
+		<?php while(shopp('category','products')): ?>
+		<?php if(shopp('category','row','products=3')): ?></ul></li><li class="row"><ul><?php endif; ?>
+			<li class="product">
+				<div class="frame">
+				<?php shopp('category','product','thumbnail&link'); ?>
+				<h4 class="name"><?php shopp('category','product','name&link'); ?></h4>
+				<p class="price"><?php shopp('category','product','price'); ?></p>
+				</div>
+			</li>
+		<?php endwhile; ?>
+		</ul></li>
+	</ul>
+	
+	<div class="clear"></div>
 <?php endif; ?>
-</div>
