87bef35cdb7cd64d3f676854b2e471fa078550dd
Jonathan Davis <jond@ingenesis.net>
2008-08-26 04:22:43 +0000

    Removed the div#shopp wrapper out of the template as it will now be included by runtime code
    git-svn-id: file:///Users/jond/svngit/svnrepos/shopp/trunk@138 846ab242-8549-0410-9a75-dfdbdae9473e

--- templates/catalog.php
+++ templates/catalog.php
@@ -1,5 +1,3 @@
-<div id="new" class="shopp">
-	<?php shopp('catalog','onsale-products','show=3&breadcrumb=false'); ?>
-	<?php shopp('catalog','featured-products','show=3&breadcrumb=false'); ?>
-	<?php shopp('catalog','new-products','show=3&breadcrumb=false'); ?>
-</div>
+<?php shopp('catalog','onsale-products','show=3&breadcrumb=false'); ?>
+<?php shopp('catalog','featured-products','show=3&breadcrumb=false'); ?>
+<?php shopp('catalog','new-products','show=3&breadcrumb=false'); ?>
