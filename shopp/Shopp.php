e70498743e6d934e5e9c2df21220e01de60e201b
Jonathan Davis <jond@ingenesis.net>
2009-01-13 02:36:50 +0000

    Enhanced the page indexing method to run every time a Shopp-page is saved, the rewrite rules are generated and when updating the reading options
    git-svn-id: file:///Users/jond/svngit/svnrepos/shopp/trunk@318 846ab242-8549-0410-9a75-dfdbdae9473e

--- shopp/Shopp.php
+++ shopp/Shopp.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Shopp
-Version: 1.0.1
+Version: 1.0.2
 Description: Bolt-on ecommerce solution for WordPress
 Plugin URI: http://shopplugin.net
 Author: Ingenesis Limited
@@ -26,7 +26,7 @@ Author URI: http://ingenesis.net
 
 */
 
-define("SHOPP_VERSION","1.0.1");
+define("SHOPP_VERSION","1.0.2");
 define("SHOPP_GATEWAY_USERAGENT","WordPress Shopp Plugin/".SHOPP_VERSION);
 define("SHOPP_HOME","http://shopplugin.net/");
 define("SHOPP_DOCS","http://docs.shopplugin.net/");
