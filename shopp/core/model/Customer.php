b49c6b2b5d8140ad390a0abd57013cb251315a18
Jonathan Davis <jond@ingenesis.net>
2009-09-09 15:54:25 +0000

    Fixed customer process-login to properly force logins on the account page, but otherwise keep it disabled until flagged by behaviors
    git-svn-id: file:///Users/jond/svngit/svnrepos/shopp/trunk@448 846ab242-8549-0410-9a75-dfdbdae9473e

--- shopp/core/model/Customer.php
+++ shopp/core/model/Customer.php
@@ -325,7 +325,9 @@ class Customer extends DatabaseObject {
 			case "submit-login": // Deprecating
 			case "login-button":
 				if (!isset($options['value'])) $options['value'] = __('Login','Shopp');
-				$string = '<input type="hidden" name="process-login" id="process-login" value="false" />';
+				if (is_shopp_page('account'))
+					$string = '<input type="hidden" name="process-login" id="process-login" value="true" />';
+				else $string = '<input type="hidden" name="process-login" id="process-login" value="false" />';
 				$string .= '<input type="submit" name="submit-login" id="submit-login"'.inputattrs($options).' />';
 				return $string;
 				break;
