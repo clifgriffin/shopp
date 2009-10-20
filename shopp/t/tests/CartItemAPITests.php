e2a2320a57b60524973742997381253b843bdcb0
John Dillick <jdillick@ingenesis.net>
2009-10-20 17:37:43 +0000

    Added additional cartitem api tests, and cleanup for Item.
    git-svn-id: file:///Users/jond/svngit/svnrepos/shopp/trunk@585 846ab242-8549-0410-9a75-dfdbdae9473e

--- shopp/t/tests/CartItemAPITests.php
+++ shopp/t/tests/CartItemAPITests.php
@@ -18,10 +18,11 @@ class CartItemAPITests extends ShoppTestCase {
 	function CartItemAPITests () {
 	}
 	
+	// example for adding product to cart by the product id and price id
 	function test_cartitem_addbypriceid () {
 		global $Shopp;
 		$Shopp->Cart->clear();
-		// print_r($Shopp->Cart->data);
+		
 		$product = new Product(55);
 
 		$price = 108;
@@ -31,10 +32,10 @@ class CartItemAPITests extends ShoppTestCase {
 
 	}
 	
+	// example for adding product to cart by the product id and the option number
 	function test_cartitem_addbyoptionid () {
 		global $Shopp;
 		$Shopp->Cart->clear();
-		// print_r($Shopp->Cart->data);
 		
 		$product = new Product(55);
 		
@@ -47,7 +48,6 @@ class CartItemAPITests extends ShoppTestCase {
 	function test_cartitem_name () {
 		global $Shopp;
 		$Shopp->Cart->clear();
-		// print_r($Shopp->Cart->data);
 		
 		$product = new Product(55);
 
@@ -56,16 +56,15 @@ class CartItemAPITests extends ShoppTestCase {
 		$this->assertTrue(shopp('cart','hasitems'));
 		ob_start();
 		while(shopp('cart', 'items')) shopp('cartitem','name');
-		$output = ob_get_contents();
+		$actual = ob_get_contents();
 		ob_end_clean();
-		
-		$this->assertEquals("Smart & Sexy - Push-Up Underwire Bra and Thong Panty Set",$output);
+		$expected = "Smart & Sexy - Push-Up Underwire Bra and Thong Panty Set";
+		$this->assertEquals($expected, $actual);
 	}
 	
 	function test_cartitem_url () {
 		global $Shopp;
 		$Shopp->Cart->clear();
-		// print_r($Shopp->Cart->data);
 		
 		$product = new Product(55);
 
@@ -74,17 +73,16 @@ class CartItemAPITests extends ShoppTestCase {
 		$this->assertTrue(shopp('cart','hasitems'));
 		ob_start();
 		while(shopp('cart', 'items')) shopp('cartitem','url');
-		$output = ob_get_contents();
+		$actual = ob_get_contents();
 		ob_end_clean();
-		// echo $output."\n";
-		$this->assertEquals("http://shopptest/store/smart-sexy-push-up-underwire-bra-and-thong-panty-set",$output);
+		$expected = "http://shopptest/store/smart-sexy-push-up-underwire-bra-and-thong-panty-set";
+		$this->assertEquals($expected, $actual);
 	}
 
 	function test_cartitem_sku () {
 		global $Shopp;
 		$Shopp->Cart->clear();
-		// print_r($Shopp->Cart->data);
-		
+				
 		$product = new Product(81);
 
 		$price = array(11);
@@ -92,16 +90,15 @@ class CartItemAPITests extends ShoppTestCase {
 		$this->assertTrue(shopp('cart','hasitems'));
 		ob_start();
 		while(shopp('cart', 'items')) shopp('cartitem','sku');
-		$output = ob_get_contents();
+		$actual = ob_get_contents();
 		ob_end_clean();
-		// echo $output."\n";
-		// $this->assertEquals("BR-81",$output);
+		$expected = "BR-81";
+		$this->assertEquals($expected, $actual);
 	}
 
 	function test_cartitem_unitprice () {
 		global $Shopp;
 		$Shopp->Cart->clear();
-		// print_r($Shopp->Cart->data);
 		
 		$product = new Product(55);
 
@@ -112,17 +109,47 @@ class CartItemAPITests extends ShoppTestCase {
 		while(shopp('cart', 'items')){ 
 			ob_start();
 			shopp('cartitem','unitprice');
-			$output = ob_get_contents();
+			$actual = ob_get_contents();
 			ob_end_clean();
 			
-			// echo $output."\n";
-			$this->assertEquals("$10.00",$output);
+			$expected = "$10.00";
+			$this->assertEquals($expected, $actual);
 			
 			ob_start();
 			shopp('cartitem', 'unitprice', 'currency=off&taxes=true');
-			$output = ob_get_contents();
+			$actual = ob_get_contents();
 			ob_end_clean();
-			$this->assertEquals("11.50",$output);
+			$expected = "11.50";
+			$this->assertEquals($expected, $actual);
+		}		
+	}
+	
+	function test_cartitem_tax () {
+		global $Shopp;
+		$Shopp->Cart->clear();
+		
+		$product = new Product(55);
+
+		$price = array(1);
+		$Shopp->Cart->add(1, $product, $price, false);
+		$this->assertTrue(shopp('cart','hasitems'));
+
+		while(shopp('cart', 'items')){ 
+			ob_start();
+			shopp('cartitem','tax');
+			$actual = ob_get_contents();
+			ob_end_clean();
+			
+			$expected = "$0.00";
+			$this->assertEquals($expected, $actual);
+			
+			ob_start();
+			shopp('cartitem','tax', 'taxes=true');
+			$actual = ob_get_contents();
+			ob_end_clean();
+			
+			$expected = "$1.50";
+			$this->assertEquals($expected, $actual);
 		}		
 	}
 	
@@ -146,13 +173,42 @@ class CartItemAPITests extends ShoppTestCase {
 			$item = current($Shopp->Cart->contents);
 			ob_start();			 
 			shopp('cartitem','quantity');
-			$output = ob_get_contents();
+			$actual = ob_get_contents();
 			ob_end_clean();
 			
-			if($item->product == 55) $this->assertEquals("1", $output);
-			else $this->assertEquals("6", $output);
+			if($item->product == 55) $this->assertEquals("1", $actual);
+			else $this->assertEquals("6", $actual);
 		}
 		
+	}
+	
+	function test_cartitem_total () {
+		global $Shopp;
+		$Shopp->Cart->clear();
+		
+		$product = new Product(55);
+
+		$price = array(1);
+		$Shopp->Cart->add(3, $product, $price, false);
+		$this->assertTrue(shopp('cart','hasitems'));
+
+		while(shopp('cart', 'items')){ 
+			ob_start();
+			shopp('cartitem','total');
+			$actual = ob_get_contents();
+			ob_end_clean();
+			
+			$expected = "$30.00";
+			$this->assertEquals($expected, $actual);
+			
+			ob_start();
+			shopp('cartitem','total', 'taxes=on&currency=off');
+			$actual = ob_get_contents();
+			ob_end_clean();
+			
+			$expected = "34.50";
+			$this->assertEquals($expected, $actual);
+		}		
 	}
 	
 	function test_cartitem_quantityinput () {
@@ -169,28 +225,28 @@ class CartItemAPITests extends ShoppTestCase {
 		while(shopp('cart', 'items')){
 			ob_start();			 
 			shopp('cartitem','quantity', 'input=menu');
-			$output = ob_get_contents();
+			$actual = ob_get_contents();
 			ob_end_clean();
 
 			ob_start();
 ?><select name="items[0][quantity]"><option selected="selected" value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option><option value="11">11</option><option value="12">12</option><option value="13">13</option><option value="14">14</option><option value="15">15</option><option value="20">20</option><option value="25">25</option><option value="30">30</option><option value="35">35</option><option value="40">40</option><option value="45">45</option><option value="50">50</option><option value="60">60</option><option value="70">70</option><option value="80">80</option><option value="90">90</option><option value="100">100</option></select><?php
-			$testValue = ob_get_contents();
+			$expected = ob_get_contents();
 			ob_end_clean();
 			
-			$this->assertEquals($testValue,$output);	
-			$this->assertValidMarkup($output);
+			$this->assertEquals($expected,$actual);	
+			$this->assertValidMarkup($actual);
 			
 			ob_start();			 
 			shopp('cartitem','quantity', 'input=text&class=myClass');
-			$output = ob_get_contents();
+			$actual = ob_get_contents();
 			ob_end_clean();
 
 			ob_start();
 			?><input type="text" name="items[0][quantity]" id="items-0-quantity"  size="5" value="1" class=" myClass"/><?php
-			$testValue = ob_get_contents();
+			$expected = ob_get_contents();
 			ob_end_clean();			
-			$this->assertEquals($testValue,$output);	
-			$this->assertValidMarkup($output);
+			$this->assertEquals($expected,$actual);	
+			$this->assertValidMarkup($actual);
 		}
 	}
 
@@ -208,28 +264,28 @@ class CartItemAPITests extends ShoppTestCase {
 			while(shopp('cart', 'items')){
 				ob_start();			 
 				shopp('cartitem','remove', 'input=button&label=My Label&class=myclass');
-				$output = ob_get_contents();
+				$actual = ob_get_contents();
 				ob_end_clean();
 				
 				ob_start();
 				?><button type="submit" name="remove[0]" value="0" class="myclass" tabindex="">My Label</button><?php
-				$testValue = ob_get_contents();
+				$expected = ob_get_contents();
 				ob_end_clean();
 
-				$this->assertEquals($testValue,$output);	
-				$this->assertValidMarkup($output);
+				$this->assertEquals($expected,$actual);	
+				$this->assertValidMarkup($actual);
 
 				ob_start();			 
 				shopp('cartitem','remove', 'label=My Label 2&class=myclass2');
-				$output = ob_get_contents();
+				$actual = ob_get_contents();
 				ob_end_clean();
 				
 				ob_start();
 				?><a href="http://shopptest/store/cart/?cart=update&amp;item=0&amp;quantity=0" class="myclass2">My Label 2</a><?php
-				$testValue = ob_get_contents();
+				$expected = ob_get_contents();
 				ob_end_clean();			
-				$this->assertEquals($testValue,$output);
-				$this->assertValidMarkup($output);
+				$this->assertEquals($expected,$actual);
+				$this->assertValidMarkup($actual);
 			}
 		}
 		
@@ -245,16 +301,16 @@ class CartItemAPITests extends ShoppTestCase {
 			while(shopp('cart', 'items')){
 				ob_start();			 
 				shopp('cartitem','thumbnail', 'class=cart-thumb&width=200&height=220');
-				$output = ob_get_contents();
+				$actual = ob_get_contents();
 				ob_end_clean();
 				
 				ob_start();
 				?><img src="http://shopptest/store/images/276" alt="Smart &amp; Sexy - Push-Up Underwire Bra and Thong Panty Set thumbnail" width="200" height="220"  class="cart-thumb" /><?php
-				$testValue = ob_get_contents();
+				$expected = ob_get_contents();
 				ob_end_clean();
 				
-				$this->assertEquals($testValue, $output);
-				$this->assertValidMarkup($output);
+				$this->assertEquals($expected, $actual);
+				$this->assertValidMarkup($actual);
 			}
 			$Shopp->Cart->clear();
 			$product = new Product(9);
@@ -265,16 +321,16 @@ class CartItemAPITests extends ShoppTestCase {
 			while(shopp('cart', 'items')){
 				ob_start();			 
 				shopp('cartitem','thumbnail');
-				$output = ob_get_contents();
+				$actual = ob_get_contents();
 				ob_end_clean();
 				
 				ob_start();
 				?><img src="http://shopptest/store/images/72" alt="Faded Glory - Men's Original Fit Jeans thumbnail" width="96" height="96"  /><?php
-				$testValue = ob_get_contents();
+				$expected = ob_get_contents();
 				ob_end_clean();
 				
-				$this->assertEquals($testValue, $output);
-				$this->assertValidMarkup($output);
+				$this->assertEquals($expected, $actual);
+				$this->assertValidMarkup($actual);
 			}			
 				
 		}
@@ -291,17 +347,95 @@ class CartItemAPITests extends ShoppTestCase {
 			while(shopp('cart', 'items')){
 				ob_start();			 
 				shopp('cartitem','options','before=<div>&after=</div>');
-				$output = ob_get_contents();
+				$actual = ob_get_contents();
 				ob_end_clean();
 				
 				ob_start();
 				?><div><input type="hidden" name="items[0][product]" value="55"/> <select name="items[0][price]" id="items-0-price"><option value="108" selected="selected">34A</option><option value="109">34B</option><option value="110">36B</option><option value="111">36C</option><option value="112">38C</option></select></div><?php
-				$testValue = ob_get_contents();
+				$expected = ob_get_contents();
 				ob_end_clean();
 				
-				$this->assertEquals($testValue, $output);
-				$this->assertValidMarkup($output);
+				$this->assertEquals($expected, $actual);
+				$this->assertValidMarkup($actual);
 			}			
+		}
+
+		function test_cartitem_inputs (){
+			global $Shopp;
+			$Shopp->Cart->clear();
+
+			$product = new Product(55);
+
+			$price = 108;
+			$data = array('customField1' => 'Custom Value1\nWith Newline', 'customField2' => 'Custom Value2');
+			$Shopp->Cart->add(1, $product, $price, false, $data);
+			$this->assertTrue(shopp('cart','hasitems'));
+			while(shopp('cart', 'items')){
+				$this->assertTrue(shopp('cartitem', 'hasinputs'));
+				while(shopp('cartitem', 'inputs')) {
+					ob_start();			 
+					shopp('cartitem', 'input', 'name');
+					$name = ob_get_contents();
+					ob_end_clean();
+										
+					ob_start();			 
+					shopp('cartitem', 'input');
+					$actual = ob_get_contents();
+					ob_end_clean();
+					
+					$this->assertTrue(isset($name));
+					$expected = $data[$name];
+
+					$this->assertEquals($expected, $actual);	
+				}
+			}			
+		}
+		
+		function test_cartitem_inputslist (){
+			global $Shopp;
+			$Shopp->Cart->clear();
+
+			$product = new Product(55);
+
+			$price = 108;
+			$data = array('customField1' => 'Custom Value1\nWith Newline', 'customField2' => 'Custom Value2', 'merryxmas'=>'Merry Christmas');
+			$Shopp->Cart->add(1, $product, $price, false, $data);
+					
+			$this->assertTrue(shopp('cart','hasitems'));
+			while(shopp('cart', 'items')){
+				$this->assertTrue(shopp('cartitem', 'hasinputs'));
+				while(shopp('cartitem', 'inputs')) {
+					$this->assertTrue(shopp('cartitem', 'hasinputs'));
+					ob_start();			 
+					shopp('cartitem', 'inputslist');
+					$actual = ob_get_contents();
+					ob_end_clean();
+					$this->assertTrue(!empty($actual));
+					
+					ob_start();					
+					?><ul><li><strong>customField1</strong>: Custom Value1\nWith Newline</li><li><strong>customField2</strong>: Custom Value2</li><li><strong>merryxmas</strong>: Merry Christmas</li></ul><?php	
+					$expected = ob_get_contents();
+					ob_end_clean();
+					
+					$this->assertEquals($expected, $actual);
+					$this->assertValidMarkup($actual);
+					
+					$this->assertTrue(shopp('cartitem', 'hasinputs'));
+					ob_start();			 
+					shopp('cartitem', 'inputslist','before=<div>&after=</div>&class=customdata&exclude=merryxmas');
+					$actual = ob_get_contents();
+					ob_end_clean();
+					$this->assertTrue(!empty($actual));
+					
+					ob_start();					
+					?><div><ul class="customdata"><li><strong>customField1</strong>: Custom Value1\nWith Newline</li><li><strong>customField2</strong>: Custom Value2</li></ul></div><?php	
+					$expected = ob_get_contents();
+					ob_end_clean();
+					
+					$this->assertEquals($expected, $actual);
+					$this->assertValidMarkup($actual);
+				}
+			}		
 		}
 
 } // end CartItemAPITests class
