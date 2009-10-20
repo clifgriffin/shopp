cf2debf5c9a0669c329f406cb9e30af3e74d626a
Jonathan Davis <jond@ingenesis.net>
2009-10-20 19:27:33 +0000

    Added unit tests for the Shipping tags template API
    git-svn-id: file:///Users/jond/svngit/svnrepos/shopp/trunk@586 846ab242-8549-0410-9a75-dfdbdae9473e

---
+++ t/tests/ShippingAPITests.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * ShippingAPITests
+ * 
+ *
+ * @author Jonathan Davis
+ * @version 1.0
+ * @copyright Ingenesis Limited, 20 October, 2009
+ * @package 
+ **/
+
+/**
+ * Initialize
+ **/
+
+class ShippingAPITests extends ShoppTestCase {
+
+	function test_shipping_hasestimates () {
+		global $Shopp;
+		$Shopp->Cart->clear();
+	
+		$Product = new Product(81); $Price = false;
+		$Shopp->Cart->add(1,$Product,$Price,false);
+		$Shopp->Cart->totals();
+		$this->assertTrue(shopp('shipping','hasestimates'));
+		
+	}
+
+	function test_shipping_methodname () {
+		global $Shopp;
+		$Shopp->Cart->clear();
+	
+		$Product = new Product(81); $Price = false;
+		$Shopp->Cart->add(1,$Product,$Price,false);
+		$Shopp->Cart->totals();
+		
+		ob_start();
+		if (shopp('shipping','hasestimates'))
+			while(shopp('shipping','methods'))
+				shopp('shipping','method-name');
+		$actual = ob_get_contents();
+		ob_end_clean();
+		$this->assertEquals('Standard',$actual);
+	}
+
+	function test_shipping_methodcost () {
+		global $Shopp;
+		$Shopp->Cart->clear();
+	
+		$Product = new Product(81); $Price = false;
+		$Shopp->Cart->add(1,$Product,$Price,false);
+		$Shopp->Cart->totals();
+		
+		ob_start();
+		if (shopp('shipping','hasestimates'))
+			while(shopp('shipping','methods'))
+				shopp('shipping','method-cost');
+		$actual = ob_get_contents();
+		ob_end_clean();
+		$this->assertEquals('$3.00',$actual);
+	}
+
+	function test_shipping_methodselector () {
+		global $Shopp;
+		$Shopp->Cart->clear();
+	
+		$Product = new Product(81); $Price = false;
+		$Shopp->Cart->add(1,$Product,$Price,false);
+		$Shopp->Cart->totals();
+		
+		ob_start();
+		if (shopp('shipping','hasestimates')) shopp('shipping','methods');
+		shopp('shipping','method-selector');
+		$actual = ob_get_contents();
+		ob_end_clean();
+		
+		$expected = array(
+			'tag' => 'input',
+			'attributes' => array('type' => 'radio','name' => 'shipmethod','value' => 'Standard','class' => 'shipmethod')
+		);
+		$this->assertTag($expected,$actual,'',true);
+	}
+	
+	function test_shipping_methoddelivery () {
+		global $Shopp;
+		$Shopp->Cart->clear();
+	
+		$Product = new Product(81); $Price = false;
+		$Shopp->Cart->add(1,$Product,$Price,false);
+		$Shopp->Cart->totals();
+		
+		ob_start();
+		if (shopp('shipping','hasestimates')) shopp('shipping','methods');
+		shopp('shipping','method-delivery');
+		$actual = ob_get_contents();
+		ob_end_clean();
+		$this->assertTrue(!empty($actual));
+	}
+	
+
+} // end ShippingAPITests class
+
+?>
