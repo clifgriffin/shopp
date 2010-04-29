b9c92261ef3499912aa8215a399101127e42e703
John Dillick <jdillick@ingenesis.net>
2010-04-29 14:03:34 +0000

    Completed encryption key import/export for Manual Processing gateway.
    git-svn-id: file:///Users/jond/svngit/svnrepos/shopp/trunk@809 846ab242-8549-0410-9a75-dfdbdae9473e

--- shopp/gateways/ManualProcess/util/private.php
+++ shopp/gateways/ManualProcess/util/private.php
@@ -1,14 +1,11 @@
 <?php
-
-if($_REQUEST['download_pkey'] && $_REQUEST['private']) {
-		header("Content-type: application/octet-stream");
-		header("Content-Disposition: attachment; filename=\"private.pem\""); 
-//print_r(urldecode($_REQUEST['private']));
-		$data = base64_encode(urldecode($_REQUEST['private']));
-		echo "-----BEGIN SHOPP PRIVATE KEY-----\n";
-		echo $data;
-		echo "\n-----END SHOPP PRIVATE KEY-----";
-
+if($_REQUEST['private']) {
+	//error_log("private: ".$_REQUEST['private']);
+	header("Content-type: application/octet-stream");
+	header("Content-Disposition: attachment; filename=\"private.pem\""); 
+	$data = $_REQUEST['private'];
+	echo "-----BEGIN SHOPP PRIVATE KEY-----\n";
+	echo urlencode($data);
+	echo "\n-----END SHOPP PRIVATE KEY-----";
 }
-
 ?>
