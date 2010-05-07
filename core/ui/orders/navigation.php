126d17a37a8a4ffc0c7e4e268744af6d4caead70
Jonathan Davis <jond@ingenesis.net>
2010-05-07 13:39:46 +0000

    Removed old heading
    git-svn-id: file:///Users/jond/svngit/svnrepos/shopp/trunk@841 846ab242-8549-0410-9a75-dfdbdae9473e

--- core/ui/orders/navigation.php
+++ core/ui/orders/navigation.php
@@ -1,6 +1,3 @@
-<!-- <ul class="subsubsub right">
-	<li><a href="?page=shopp/customers"><?php _e('Customers','Shopp'); ?></a></li>
-</ul> -->
 <ul class="subsubsub">
 	<li><a href="<?php echo esc_url(add_query_arg(array_merge($_GET,array('status'=>null,'id'=>null)),admin_url('admin.php'))); ?>"><?php _e('All Orders','Shopp'); ?></a></li>
 	<?php 
