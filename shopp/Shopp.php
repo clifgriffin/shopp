863f36d19ec4214f96ec0c37395fbd0306a14411
Jonathan Davis <jond@ingenesis.net>
2010-05-11 15:19:00 +0000

    Fixed canonical redirects for catalog URLs in WP 3.0 [#172 state:closed]
    git-svn-id: file:///Users/jond/svngit/svnrepos/shopp/trunk@853 846ab242-8549-0410-9a75-dfdbdae9473e

--- shopp/Shopp.php
+++ shopp/Shopp.php
@@ -181,6 +181,7 @@ class Shopp {
 		add_filter('rewrite_rules_array',array(&$this,'rewrites'));
 		add_action('save_post', array(&$this, 'pages_index'),10,2);
 		add_filter('query_vars', array(&$this,'queryvars'));
+		add_filter('redirect_canonical', array(&$this,'canonical_home'));
 		
 		// Extras & Integrations
 		add_filter('aioseop_canonical_url', array(&$this,'canonurls'));
@@ -188,6 +189,25 @@ class Shopp {
 		if (!wp_next_scheduled('shopp_check_updates'))
 			wp_schedule_event(time(),'twicedaily','shopp_check_updates');
 
+	}
+	
+	/**
+	 * Cancels canonical redirects when the catalog is Set as the front page
+	 *
+	 * Added for WordPress 3.0 compatibility {@see wp-includes/canonical.php line 111}
+	 * 
+	 * @author Jonathan Davis
+	 * @since 1.1
+	 * 
+	 * @param string $redirect The redirected URL
+	 * @return mixed False when the Shopp catalog is set as the front page
+	 **/
+	function canonical_home ($redirect) {
+		$pages = $this->Settings->get('pages');
+
+		if ($redirect == home_url('/') && $pages['catalog']['id'] == get_option('page_on_front'))
+			return false;
+		return $redirect;
 	}
 	
 	/**
@@ -342,7 +362,6 @@ class Shopp {
 
 		// Add mod_rewrite rule for image server for low-resource, speedy delivery
 		add_rewrite_rule('.*/images/(\d+)[/\?]?(.*?)$',join('/',$corepath).'/image.php?siid=$1&$2');
-
 		return $rules + $wp_rewrite_rules;
 	}
 	
