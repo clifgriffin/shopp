e310e47dd834ddbc7839228ef556c7c831dcc865
Jonathan Davis <jond@ingenesis.net>
2010-05-26 21:18:41 +0000

    Fixed the product manager subsubsub menu navigation to format appropriately and hide the inventory menu when appropriate
    git-svn-id: file:///Users/jond/svngit/svnrepos/shopp/trunk@900 846ab242-8549-0410-9a75-dfdbdae9473e

--- core/ui/products/navigation.php
+++ core/ui/products/navigation.php
@@ -1,5 +1,5 @@
 <ul class="subsubsub">
-	<?php foreach($subs as $name => $sub): if ($name="inventory" and $sub['total'] == "0") continue; ?>
-	<li><?php echo ($name != "all")?"| ":""; ?><a href="<?php echo esc_url(add_query_arg(array_merge($_GET,array('f'=>$sub['request'])),admin_url('admin.php'))); ?>"><?php echo $sub['label']; ?></a> (<?php echo $sub['total']; ?>)</li>
+	<?php foreach($subs as $nav => $sub): if ($nav=="inventory" and $sub['total'] == "0") continue; ?>
+	<li><?php echo ($nav != "all")?"| ":""; ?><a href="<?php echo esc_url(add_query_arg(array_merge($_GET,array('f'=>$sub['request'])),admin_url('admin.php'))); ?>"><?php echo $sub['label']; ?></a> (<?php echo $sub['total']; ?>)</li>
 	<?php endforeach; ?>
 </ul>
