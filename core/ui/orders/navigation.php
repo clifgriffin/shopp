c526f8fc37299d66f400e3cbefe45656c3dc3cd1
Jonathan Davis <jond@ingenesis.net>
2010-07-27 15:01:13 +0000

    Fixed order status sub-menus not displaying when viewing an individual order
    git-svn-id: file:///Users/jond/svngit/svnrepos/shopp/trunk@1099 846ab242-8549-0410-9a75-dfdbdae9473e

--- core/ui/orders/navigation.php
+++ core/ui/orders/navigation.php
@@ -1,8 +1,8 @@
 <ul class="subsubsub">
 	<li><a href="<?php echo esc_url(add_query_arg(array_merge($_GET,array('status'=>null,'id'=>null)),admin_url('admin.php'))); ?>"><?php _e('All Orders','Shopp'); ?></a></li>
 	<?php 
-		$StatusCounts = $this->status_counts();
-		if (!empty($statusLabels)) foreach($statusLabels as $id => $label): ?>
-		<li>| <a href="<?php echo esc_url(add_query_arg(array_merge($_GET,array('status'=>$id,'id'=>null)),admin_url('admin.php'))); ?>"><?php echo $label; ?></a> (<?php echo $StatusCounts[$id]; ?>)</li>
+		$counts = $this->status_counts();
+		if (!empty($counts)) foreach($counts as $id => $status): ?>
+		<li>| <a href="<?php echo esc_url(add_query_arg(array_merge($_GET,array('status'=>$id,'id'=>null)),admin_url('admin.php'))); ?>"><?php echo $status->label; ?></a> (<?php echo $status->total; ?>)</li>
 	<?php endforeach; ?>
 </ul>
