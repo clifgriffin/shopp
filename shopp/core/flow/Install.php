d3da26508e37128eb553fa2092c304530669fc34
John Dillick <jdillick@ingenesis.net>
2010-08-17 14:59:47 +0000

    Corrected missing translation wrappers. [#397 state:closed]
    git-svn-id: file:///Users/jond/svngit/svnrepos/shopp/trunk@1197 846ab242-8549-0410-9a75-dfdbdae9473e

--- shopp/core/flow/Install.php
+++ shopp/core/flow/Install.php
@@ -735,7 +735,7 @@ class ShoppCore_Upgrader extends Shopp_Upgrader {
 		$this->strings['up_to_date'] = __('Shopp is at the latest version.','Shopp');
 		$this->strings['no_package'] = __('Shopp upgrade package not available.','Shopp');
 		$this->strings['downloading_package'] = sprintf(__('Downloading update from <span class="code">%s</span>.'),SHOPP_HOME);
-		$this->strings['unpack_package'] = __('Unpacking the update.');
+		$this->strings['unpack_package'] = __('Unpacking the update.','Shopp');
 		$this->strings['deactivate_plugin'] = __('Deactivating Shopp.','Shopp');
 		$this->strings['remove_old'] = __('Removing the old version of Shopp.','Shopp');
 		$this->strings['remove_old_failed'] = __('Could not remove the old Shopp.','Shopp');
