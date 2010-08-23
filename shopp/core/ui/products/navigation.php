b44b4884b849481ebb787dcb4ec2a59b10b60075
Jonathan Davis <jond@ingenesis.net>
2010-08-23 20:10:14 +0000

    Fixed an unset array index notice in the product manager sub navigation Removed a javascript console log message that would cause problems in browsers without a console
    git-svn-id: file:///Users/jond/svngit/svnrepos/shopp/trunk@1234 846ab242-8549-0410-9a75-dfdbdae9473e

--- shopp/core/ui/products/navigation.php
+++ shopp/core/ui/products/navigation.php
@@ -1,5 +1,8 @@
 <ul class="subsubsub">
-	<?php foreach($subs as $nav => $sub): if ($nav=="inventory" and $sub['total'] == "0") continue; ?>
-	<li><?php echo ($nav != "all")?"| ":""; ?><a href="<?php echo esc_url(add_query_arg(array_merge($_GET,array('f'=>$sub['request'])),admin_url('admin.php'))); ?>"><?php echo $sub['label']; ?></a> (<?php echo $sub['total']; ?>)</li>
+	<?php foreach($subs as $nav => $sub): 
+		if ($nav=="inventory" and $sub['total'] == "0") continue; 
+		$filter = isset($sub['request'])?array('f'=>$sub['request']):array('f'=>null);
+	?>
+	<li><?php echo ($nav != "all")?"| ":""; ?><a href="<?php echo esc_url(add_query_arg(array_merge($_GET,$filter),admin_url('admin.php'))); ?>"><?php echo $sub['label']; ?></a> (<?php echo $sub['total']; ?>)</li>
 	<?php endforeach; ?>
 </ul>
