2a071c52ec07adaf9de452e9d9bc21a68f23eeb0
John Dillick <jdillick@ingenesis.net>
2010-08-25 21:00:09 +0000

    Fixed problem where breadcrumbs didn't work because the category uri for new categories was not saved correctly for categories with no products. [#378 state:review]
    git-svn-id: file:///Users/jond/svngit/svnrepos/shopp/trunk@1254 846ab242-8549-0410-9a75-dfdbdae9473e

--- shopp/core/model/Catalog.php
+++ shopp/core/model/Catalog.php
@@ -67,10 +67,7 @@ class Catalog extends DatabaseObject {
 		
 		if (!is_array($where)) $where = array($where);
 
-		// Merge joins
-		if (isset($loading['joins'])) $joins = array_merge($defaults['joins'],$loading['joins']);
-
-		if (!$this->outofstock) $where[] = "(pt.inventory='off' OR (pt.inventory='on' AND pt.stock > 0))";
+		if (!$outofstock) $where[] = "(pt.inventory='off' OR (pt.inventory='on' AND pt.stock > 0))";
 
 		if ($parent !== false) $where[] = "cat.parent=".$parent;
 		else $parent = 0;
