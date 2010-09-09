7fc1b5edd99cfada25eb29721e50445446c2527d
Jonathan Davis <jond@ingenesis.net>
2010-09-09 19:35:51 +0000

    Fixed the security redirect for the checkout process pages (checkout, confirm-order, thanks)
    git-svn-id: file:///Users/jond/svngit/svnrepos/shopp/trunk@1341 846ab242-8549-0410-9a75-dfdbdae9473e

--- shopp/core/flow/Storefront.php
+++ shopp/core/flow/Storefront.php
@@ -130,7 +130,7 @@ class Storefront extends FlowController {
 		global $Shopp;
 		if (is_shopp_secure() || !$Shopp->Gateways->secure) return;
 		switch ($this->Page['name']) {
-			case "checkout": shopp_redirect(shoppurl($_GET,'checkout',true)); break;
+			case "checkout": shopp_redirect(shoppurl($_GET,get_query_var('shopp_proc'),true)); break;
 			case "account":	 shopp_redirect(shoppurl($_GET,'account',true)); break;
 		}
 	}
