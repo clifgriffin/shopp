49f82bd28ff4c1799a8ade66d05b96524df21818
Jonathan Davis <jond@ingenesis.net>
2010-12-01 13:13:29 +0000

    Fixed order status counts to line up correctly [#684 state:review] Fixed a PHP error occurring on the orders manager [#644 state:closed]
    git-svn-id: file:///Users/jond/svngit/svnrepos/shopp/trunk@1499 846ab242-8549-0410-9a75-dfdbdae9473e

--- shopp/core/ui/orders/navigation.php
+++ shopp/core/ui/orders/navigation.php
@@ -2,7 +2,7 @@
 	<li><a href="<?php echo esc_url(add_query_arg(array_merge($_GET,array('status'=>null,'id'=>null)),admin_url('admin.php'))); ?>"><?php _e('All Orders','Shopp'); ?></a></li>
 	<?php 
 		$counts = $this->status_counts();
-		if (!empty($counts)) foreach($counts as $id => $status): ?>
-		<li>| <a href="<?php echo esc_url(add_query_arg(array_merge($_GET,array('status'=>$id,'id'=>null)),admin_url('admin.php'))); ?>"><?php echo $status->label; ?></a> (<?php echo $status->total; ?>)</li>
+		if (!empty($counts)) foreach($counts as $id => $state): ?>
+		<li>| <a href="<?php echo esc_url(add_query_arg(array_merge($_GET,array('status'=>$id,'id'=>null)),admin_url('admin.php'))); ?>"><?php echo $state->label; ?></a> (<?php echo $state->total; ?>)</li>
 	<?php endforeach; ?>
 </ul>
