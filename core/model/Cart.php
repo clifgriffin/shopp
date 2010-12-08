379daa14ba51d55165f33378a6a0eff8327c4784
Jonathan Davis <jond@ingenesis.net>
2010-12-08 18:21:28 +0000

    Corrected the shopp('shipping','url') to use the confirm-order URL when in the checkout process, or the cart URL by default [#661 state:closed]
    git-svn-id: file:///Users/jond/svngit/svnrepos/shopp/trunk@1519 846ab242-8549-0410-9a75-dfdbdae9473e

--- core/model/Cart.php
+++ core/model/Cart.php
@@ -874,7 +874,7 @@ class Cart {
 		$result = "";
 		
 		switch ($property) {
-			case "url": return shoppurl(false,'cart');
+			case "url": return is_shopp_page('checkout')?shoppurl(false,'confirm-order'):shoppurl(false,'cart');
 			case "hasestimates": return apply_filters('shopp_shipping_hasestimates',!empty($this->shipping)); break;
 			case "options":
 			case "methods":
