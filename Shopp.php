4fdaf733940f028ac8b6f29a6dbda1a81888d17c
Jonathan Davis <jond@ingenesis.net>
2011-01-26 21:25:08 +0000

    Version 1.1.6 for release
    git-svn-id: file:///Users/jond/svngit/svnrepos/shopp/branches/1.1@1690 846ab242-8549-0410-9a75-dfdbdae9473e

--- Shopp.php
+++ Shopp.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Shopp
-Version: 1.1.6dev
+Version: 1.1.6
 Description: Bolt-on ecommerce solution for WordPress
 Plugin URI: http://shopplugin.net
 Author: Ingenesis Limited
@@ -26,7 +26,7 @@ Author URI: http://ingenesis.net
 
 */
 
-define('SHOPP_VERSION','1.1.6dev');
+define('SHOPP_VERSION','1.1.6');
 define('SHOPP_REVISION','$Rev$');
 define('SHOPP_GATEWAY_USERAGENT','WordPress Shopp Plugin/'.SHOPP_VERSION);
 define('SHOPP_HOME','https://shopplugin.net/');
