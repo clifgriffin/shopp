c55f597b764b1776c71e0385902008f28308c173
Jonathan Davis <jond@ingenesis.net>
2011-03-03 14:35:13 +0000

    Fixed an undefined variable reference in str_true()
    git-svn-id: file:///Users/jond/svngit/svnrepos/shopp/trunk@1750 846ab242-8549-0410-9a75-dfdbdae9473e

--- shopp/core/functions.php
+++ shopp/core/functions.php
@@ -1962,7 +1962,7 @@ function sort_tree ($items,$parent=0,$key=-1,$depth=-1) {
  * @return boolean The boolean value of the provided text
  **/
 function str_true ( $string, $istrue = array('yes', 'y', 'true','1','on') ) {
-	if (is_bool($value)) return $string;
+	if (is_bool($string)) return $string;
 	return in_array(strtolower($string),$istrue);
 }
 
