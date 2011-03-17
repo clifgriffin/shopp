c810049079efa16a988842a55b9d6301d513819d
John Dillick <jdillick@ingenesis.net>
2011-03-17 14:59:01 +0000

    Fixed fatal error when loading scripts through WordPress.
    git-svn-id: file:///Users/jond/svngit/svnrepos/shopp/trunk@1775 846ab242-8549-0410-9a75-dfdbdae9473e

--- shopp/core/flow/Flow.php
+++ shopp/core/flow/Flow.php
@@ -13,11 +13,11 @@
 
 // Image Server request handling
 if (isset($_GET['siid']) || preg_match('/images\/\d+/',$_SERVER['REQUEST_URI']))
-	require(SHOPP_PATH.'/core/image.php');
+	require(dirname(dirname(__FILE__)).'/image.php');
 
 // Script Server request handling
 if (isset($_GET['sjsl']))
-	require(SHOPP_PATH.'/core/scripts.php');
+	require(dirname(dirname(__FILE__)).'/scripts.php');
 
 /**
  * Flow
