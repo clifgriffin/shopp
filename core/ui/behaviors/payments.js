84cb7fa4ec882f37cf96894ff0b16cb9e07b1f90
Jonathan Davis <jond@ingenesis.net>
2011-06-15 20:29:20 +0000

    Enhanced add payment menu to work on change and hide the "add" button when JS is available
    git-svn-id: file:///Users/jond/svngit/svnrepos/shopp/trunk@1972 846ab242-8549-0410-9a75-dfdbdae9473e

--- core/ui/behaviors/payments.js
+++ core/ui/behaviors/payments.js
@@ -11,41 +11,45 @@ jQuery(document).ready( function($) {
 
 	var editing = false,
 		menu = $('#payment-option-menu'),
-		notice = $('#no-payment-settings');
+		notice = $('#no-payment-settings'),
 
-	$('#payments a.edit, #add-payment-option').click(function (e) {
-		e.preventDefault();
-		if (editing) editing.cancel(false);
-		notice.hide();
-		var $this = $(this),
-			row = $this.parents('tr').hide(),
-			selected = menu.val().toLowerCase(),
-			id = row.size() > 0?row.attr('id').substr(16):selected,
-			ui = $.tmpl(id+'-editor'),
-			cancel = ui.find('a.cancel'),
-			selectall = ui.find('input.selectall-toggle').change(function (e) {
-				var $this = $(this),
-					options = $this.parents('ul').find('input');
-				options.attr('checked',$this.attr('checked'));
-			});
-
-		if (row.size() == 0) row = $('#payment-setting-'+id).hide();
-		menu.get(0).selectedIndex = 0;
-
-		$this.cancel = function (e) {
+		AddPayment = function (e) {
 			if (e) e.preventDefault();
-			editing = false;
-			ui.remove();
-			row.fadeIn('fast');
-			if (notice.size() > 0) notice.show();
+			if (editing) editing.cancel(false);
+			notice.hide();
+			var $this = $(this),
+				row = $this.parents('tr').hide(),
+				selected = menu.val().toLowerCase(),
+				id = row.size() > 0?row.attr('id').substr(16):selected,
+				ui = $.tmpl(id+'-editor'),
+				cancel = ui.find('a.cancel'),
+				selectall = ui.find('input.selectall-toggle').change(function (e) {
+					var $this = $(this),
+						options = $this.parents('ul').find('input');
+					options.attr('checked',$this.attr('checked'));
+				});
+
+			if (row.size() == 0) row = $('#payment-setting-'+id).hide();
+			menu.get(0).selectedIndex = 0;
+
+			$this.cancel = function (e) {
+				if (e) e.preventDefault();
+				editing = false;
+				ui.remove();
+				row.fadeIn('fast');
+				if (notice.size() > 0) notice.show();
+			};
+			cancel.click($this.cancel);
+
+			if (row.size() > 0) ui.insertAfter(row);
+			else ui.prependTo('#payments-settings-table');
+
+			editing = $this;
+
 		};
-		cancel.click($this.cancel);
 
-		if (row.size() > 0) ui.insertAfter(row);
-		else ui.prependTo('#payments-settings-table');
-
-		editing = $this;
-	});
+	$('#payments a.edit').click(AddPayment);
+	$('#payment-option-menu').change(AddPayment);
 
 	$('#payments a.delete').click(function() {
 		if (confirm($ps.confirm)) return true;
