55daf95cc16cf37bd5454586a01b9b378aee8c56
John Dillick <jdillick@ingenesis.net>
2011-06-25 21:08:40 +0000

    Implemented plugin api for Shopp settings.
    git-svn-id: file:///Users/jond/svngit/svnrepos/shopp/trunk@2006 846ab242-8549-0410-9a75-dfdbdae9473e

--- api/settings.php
+++ api/settings.php
@@ -2,9 +2,9 @@
 /**
  * settings
  *
- * Description…
+ * plugin API for getting, setting/creating, and deleting Shopp settings.
  *
- * @author Jonathan Davis
+ * @author Jonathan Davis, John Dillick
  * @version 1.0
  * @copyright Ingenesis Limited, June 23, 2011
  * @license GNU GPL version 3 (or later) {@see license.txt}
@@ -13,10 +13,68 @@
  * @subpackage shopp
  **/
 
-function shopp_settings () {}
-function shopp_setting ($name) {}
+/**
+ * shopp_setting - returns a named Shopp setting
+ *
+ * @author John Dillick
+ * @since 1.2
+ *
+ * @param string $name The name of the setting
+ * @return mixed the value saved to the named setting, or false if not set.  returns null if empty name is provided
+ **/
+function shopp_setting ( $name ) {
+	$Settings = ShoppSettings();
+	$setting = null;
 
-function shopp_set_setting ($name,$value) {}
-function shopp_rmv_setting ($name) {}
+	if ( empty($name) ) {
+		if(SHOPP_DEBUG) new ShoppError("shopp_setting lookup failed: Setting name parameter required.",'shopp_setting',SHOPP_DEBUG_ERR);
+		return false;
+	}
+
+	$setting = $Settings->get($name);
+
+	return $setting;
+}
+
+/**
+ * shopp_set_setting - saves a name value pair as a Shopp setting
+ *
+ * @author John Dillick
+ * @since 1.2
+ *
+ * @param string $name The name of the setting that is to be stored.
+ * @param mixed $value The value saved to the named setting.
+ * @return bool true on success, false on failure.
+ **/
+function shopp_set_setting ( $name, $value ) {
+	$Settings = ShoppSettings();
+
+	if ( empty($name) ) {
+		if(SHOPP_DEBUG) new ShoppError("shopp_set_setting failed: Setting name parameter required.",'shopp_set_setting',SHOPP_DEBUG_ERR);
+		return false;
+	}
+
+	$Settings->save($name, $value);
+	return true;
+}
+
+/**
+ * shopp_rmv_setting - deletes a named setting
+ *
+ * @author John Dillick
+ * @since 1.2
+ *
+ * @param string $name Name of the Shopp setting to be deleted
+ * @return bool true on success, false on failure
+ **/
+function shopp_rmv_setting ($name) {
+	$Settings = ShoppSettings();
+
+	if ( empty($name) ) {
+		if(SHOPP_DEBUG) new ShoppError("shopp_rmv_setting failed: Setting name parameter required.",'shopp_rmv_setting',SHOPP_DEBUG_ERR);
+		return false;
+	}
+	return $Settings->delete($name);
+}
 
 ?>
