97638ad20c8e561576a9352cf10ebac907f67027
John Dillick <jdillick@ingenesis.net>
2011-06-29 20:04:56 +0000

    Document global object context setter/getter functions for the dev api.  Added ShoppCollection global Collection object setter/getter.
    git-svn-id: file:///Users/jond/svngit/svnrepos/shopp/trunk@2036 846ab242-8549-0410-9a75-dfdbdae9473e

--- shopp/api/core.php
+++ shopp/api/core.php
@@ -2,7 +2,7 @@
 /**
  * Core
  *
- * Description…
+ * Interface for getting and setting global objects.
  *
  * @author Jonathan Davis
  * @version 1.0
@@ -13,21 +13,64 @@
  * @subpackage shopp
  **/
 
-function &ShoppProduct (&$Object=false) {
+/**
+ * ShoppProduct - get and set the global Product object
+ *
+ * @author Jonathan Davis
+ * @since 1.2
+ *
+ * @param Product (optional) $Object the product object to set to the global context.
+ * @return mixed if the global Product context isn't set, bool false will be returned, otherwise the global Product object will be returned
+ **/
+function &ShoppProduct ( &$Object = false ) {
 	global $Shopp; $false = false;
 	if (empty($Shopp)) return $false;
 	if ($Object !== false) $Shopp->Product = $Object;
 	return $Shopp->Product;
 }
 
-function &ShoppCatalog (&$Object=false) {
+/**
+ * ShoppCollection - get and set the global Collection object (ie. ProductCategory, SmartCollection)
+ *
+ * @author John Dillick
+ * @since 1.2
+ *
+ * @param Collection (optional) $Object the Collection object to set to the global context.
+ * @return mixed if the global Collection context isn't set, bool false will be returned, otherwise the global Collection object will be returned
+ **/
+function &ShoppCollection ( &$Object = false ) {
+	global $Shopp; $false = false;
+	if (empty($Shopp)) return $false;
+	if ($Object !== false) $Shopp->Category = $Object;
+	return $Shopp->Category;
+}
+
+/**
+ * ShoppCatalog - get and set the global Catalog object
+ *
+ * @author John Dillick
+ * @since 1.2
+ *
+ * @param Catalog (optional) $Object the Catalog object to set to the global context.
+ * @return mixed if the global Catalog context isn't set, bool false will be returned, otherwise the global Catalog object will be returned
+ **/
+function &ShoppCatalog ( &$Object = false ) {
 	global $Shopp; $false = false;
 	if (empty($Shopp)) return $false;
 	if ($Object !== false) $Shopp->Catalog = $Object;
 	return $Shopp->Catalog;
 }
 
-function &ShoppPurchase (&$Object=false) {
+/**
+ * ShoppPurchase - get and set the global Purchase object
+ *
+ * @author John Dillick
+ * @since 1.2
+ *
+ * @param Purchase (optional) $Object the Catalog object to set to the global context.
+ * @return mixed if the global Purchase context isn't set, bool false will be returned, otherwise the global Purchase object will be returned
+ **/
+function &ShoppPurchase ( &$Object = false ) {
 	global $Shopp; $false = false;
 	if (empty($Shopp)) return $false;
 	if ($Object !== false) $Shopp->Purchase = $Object;
