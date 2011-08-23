9a49e961d2d9662208b668446d433b7adc1de733
Jonathan Davis <jond@ingenesis.net>
2011-08-23 17:17:40 +0000

    Added new flat rate and percentage-based shipping calculators
    git-svn-id: file:///Users/jond/svngit/svnrepos/shopp/trunk@2206 846ab242-8549-0410-9a75-dfdbdae9473e

---
+++ shipping/core/ItemRates.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Item Rates
+ *
+ * Provides flat rates per item
+ *
+ * @author Jonathan Davis
+ * @version 1.2
+ * @copyright Ingenesis Limited, June 14, 2011
+ * @package shopp
+ * @since 1.2 dev
+ * @subpackage ItemRates
+ *
+ **/
+
+class ItemRates extends ShippingFramework implements ShippingModule {
+
+	function methods () {
+		return __('Flat Item Rates','Shopp');
+	}
+
+	function init () {
+		$rate['items'] = array();
+	}
+
+	function calcitem ($id,$Item) {
+		foreach ($this->methods as $slug => &$method) {
+			$amount = $this->tablerate($method['table']);
+			if ($amount === false) continue; // Skip methods that don't match at all
+			$method['items'] = $Item->quantity * $amount;
+		}
+	}
+
+	function calculate ($options,$Order) {
+
+		foreach ($this->methods as $slug => $method) {
+
+			$amount = $this->tablerate($method['table']);
+			if ($amount === false) continue; // Skip methods that don't match at all
+			$rate = array(
+				'slug' => $slug,
+				'name' => $method['label'],
+				'amount' => array_sum($method['items']),
+				'delivery' => false,
+				'items' => $method['items']
+			);
+
+			$options[$slug] = new ShippingOption($rate);
+
+		}
+
+		return $options;
+	}
+
+	function settings () {
+
+		$this->ui->flatrates(0,array(
+			'table' => $this->settings['table']
+		));
+
+	}
+
+} // END class OrderRates
+?>
