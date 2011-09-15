c9e2ac42b390022504058c8f89f2fcd163ffbb8b
John Dillick <jdillick@ingenesis.net>
2011-09-15 17:13:08 +0000

    Implemented base case unit tests for all packaging algorithms (no weight or dimension limits.)
    git-svn-id: file:///Users/jond/svngit/svnrepos/shopp/trunk@2267 846ab242-8549-0410-9a75-dfdbdae9473e

--- t/tests/PackagingTests.php
+++ t/tests/PackagingTests.php
@@ -9,11 +9,10 @@
  * @package
  **/
 
-/**
- * Initialize
- **/
-
 class PackagingTests extends ShoppTestCase {
+	/**
+	 * Initialize
+	 **/
 	function setUp () {
 		parent::setUp();
 
@@ -81,6 +80,7 @@ class PackagingTests extends ShoppTestCase {
 
 	}
 
+	// testing packaging all items together, by mass
 	function test_package_mass () {
 		global $PkgProduct1, $PkgProduct2, $PkgProduct3, $PkgProduct4;
 		$products = array($PkgProduct1, $PkgProduct2, $PkgProduct3, $PkgProduct4);
@@ -181,10 +181,205 @@ class PackagingTests extends ShoppTestCase {
 		}
 	}
 
-	function test_package_like () {}
+	// testing packaging all like items together
+	function test_package_like () {
+		global $PkgProduct1, $PkgProduct2, $PkgProduct3, $PkgProduct4;
+		$products = array($PkgProduct1, $PkgProduct2, $PkgProduct3, $PkgProduct4);
+		$items = array();
+		for ($i = 0; $i < ( 2 * count($products) ); $i++ ) {
+			$p = $i % count($products);
+			if ( isset($items[$p]) ) $items[$p]->quantity($items[$p]->quantity + ($p + 1) * ($i % 3 + 1) );
+			else {
+				$items[$p] = new Item($products[$p], false);
+				$items[$p]->quantity($i + 1);
+			}
+		}
 
-	function test_package_all () {}
+		$packager = new ShippingPackager(array('type'=>'like'));
+		// echo "\nItems\n";
+		foreach ( $items as $item ) {
+			// echo "item $item->name - qty: $item->quantity Each wt: $item->weight h: $item->height w: $item->width l: $item->length val: $item->unitprice\n";
+			$packager->add_item($item);
+		}
+		// echo "\n";
 
-	function test_package_piece () {}
+		$pkgs = array();
+		while ( $packager->packages() ) {
+			$pkgs[] = $p = $packager->package();
+			// echo "Package ".count($pkgs).":\nItems:\n";
+			// foreach ( $p->contents() as $item ) {
+			// 	echo "Item '$item->name' qty($item->quantity) - Each wt: $item->weight h: $item->height w: $item->width l: $item->length val: $item->unitprice\n";
+			// }
+			// echo sprintf(
+			// 	"Dimensions (w x l x h): %d x %d x %d\tWeight: %d\tValue: %d".
+			// 	"\n---------------------------------------------------------\n",
+			// 	$p->width(), $p->length(), $p->height(), $p->weight(), $p->value());
+			$pc = count($pkgs);
+			switch ( $pc ) {
+				case 1:
+				case 2:
+				case 3:
+					$this->AssertEquals( 1, count($p->contents()));
+					$this->AssertEquals( 1, $p->weight());
+					$this->AssertEquals( 1, $p->width());
+					$this->AssertEquals( 1, $p->length());
+					$this->AssertEquals( 1, $p->height());
+					$this->AssertEquals( 41, $p->value());
+					break;
+				case 4:
+					$this->AssertEquals( 1, count($p->contents()));
+					$this->AssertEquals( 80, $p->weight());
+					$this->AssertEquals( 5, $p->width());
+					$this->AssertEquals( 5, $p->length());
+					$this->AssertEquals( 40, $p->height());
+					$this->AssertEquals( 336, $p->value());
+					break;
+				case 5:
+					$this->AssertEquals( 1, count($p->contents()));
+					$this->AssertEquals( 90, $p->weight());
+					$this->AssertEquals( 5, $p->width());
+					$this->AssertEquals( 15, $p->length());
+					$this->AssertEquals( 30, $p->height());
+					$this->AssertEquals( 252, $p->value());
+					break;
+				case 6:
+					$this->AssertEquals( 1, count($p->contents()));
+					$this->AssertEquals( 600, $p->weight());
+					$this->AssertEquals( 10, $p->width());
+					$this->AssertEquals( 10, $p->length());
+					$this->AssertEquals( 240, $p->height());
+					$this->AssertEquals( 504, $p->value());
+			}
+		}
+	}
+
+	// testing packaging all items together, with dimensions
+	function test_package_all () {
+		global $PkgProduct1, $PkgProduct2, $PkgProduct3, $PkgProduct4;
+		$products = array($PkgProduct1, $PkgProduct2, $PkgProduct3, $PkgProduct4);
+		$items = array();
+		for ($i = 0; $i < ( 2 * count($products) ); $i++ ) {
+			$p = $i % count($products);
+			if ( isset($items[$p]) ) $items[$p]->quantity($items[$p]->quantity + ($p + 1) * ($i % 5 + 1) );
+			else {
+				$items[$p] = new Item($products[$p], false);
+				$items[$p]->quantity($i + 1);
+			}
+		}
+
+		$packager = new ShippingPackager(array('type'=>'all'));
+		// echo "\nItems\n";
+		foreach ( $items as $item ) {
+			// echo "item $item->name - qty: $item->quantity Each wt: $item->weight h: $item->height w: $item->width l: $item->length val: $item->unitprice\n";
+			$packager->add_item($item);
+		}
+		// echo "\n";
+
+		$pkgs = array();
+		while ( $packager->packages() ) {
+			$pkgs[] = $p = $packager->package();
+			// echo "Package ".count($pkgs).":\nItems:\n";
+			// foreach ( $p->contents() as $item ) {
+			// 	echo "Item '$item->name' qty($item->quantity) - Each wt: $item->weight h: $item->height w: $item->width l: $item->length val: $item->unitprice\n";
+			// }
+			// echo sprintf(
+			// 	"Dimensions (w x l x h): %d x %d x %d\tWeight: %d\tValue: %d".
+			// 	"\n---------------------------------------------------------\n",
+			// 	$p->width(), $p->length(), $p->height(), $p->weight(), $p->value());
+			$pc = count($pkgs);
+			switch ( $pc ) {
+				case 1:
+				case 2:
+				case 3:
+				case 4:
+				case 5:
+				case 6:
+					$this->AssertEquals( 1, count($p->contents()));
+					$this->AssertEquals( 1, $p->weight());
+					$this->AssertEquals( 1, $p->width());
+					$this->AssertEquals( 1, $p->length());
+					$this->AssertEquals( 1, $p->height());
+					$this->AssertEquals( 41, $p->value());
+					break;
+				case 7:
+					$this->AssertEquals( 3, count($p->contents()));
+					$this->AssertEquals( 975, $p->weight());
+					$this->AssertEquals( 10, $p->width());
+					$this->AssertEquals( 15, $p->length());
+					$this->AssertEquals( 385, $p->height());
+					$this->AssertEquals( 1218, $p->value());
+			}
+		}
+	}
+
+	// packaging all items in separate packages
+	function test_package_piece () {
+		global $PkgProduct1, $PkgProduct2, $PkgProduct3, $PkgProduct4;
+		$products = array($PkgProduct1, $PkgProduct2, $PkgProduct3, $PkgProduct4);
+		$items = array();
+		for ($i = 0; $i < count($products); $i++ ) {
+				$items[$i] = new Item($products[$i], false);
+				$items[$i]->quantity( max( 1, (6 - $i) % 4 ) );
+		}
+
+		$packager = new ShippingPackager(array('type'=>'piece'));
+		// echo "\nItems\n";
+		foreach ( $items as $item ) {
+			// echo "item $item->name - qty: $item->quantity Each wt: $item->weight h: $item->height w: $item->width l: $item->length val: $item->unitprice\n";
+			$packager->add_item($item);
+		}
+		// echo "\n";
+
+		$pkgs = array();
+		while ( $packager->packages() ) {
+			$pkgs[] = $p = $packager->package();
+			// echo "Package ".count($pkgs).":\nItems:\n";
+			// foreach ( $p->contents() as $item ) {
+			// 	echo "Item '$item->name' qty($item->quantity) - Each wt: $item->weight h: $item->height w: $item->width l: $item->length val: $item->unitprice\n";
+			// }
+			// echo sprintf(
+			// 	"Dimensions (w x l x h): %d x %d x %d\tWeight: %d\tValue: %d".
+			// 	"\n---------------------------------------------------------\n",
+			// 	$p->width(), $p->length(), $p->height(), $p->weight(), $p->value());
+			$pc = count($pkgs);
+			switch ( $pc ) {
+				case 1:
+				case 2:
+					$this->AssertEquals( 1, count($p->contents()));
+					$this->AssertEquals( 1, $p->weight());
+					$this->AssertEquals( 1, $p->width());
+					$this->AssertEquals( 1, $p->length());
+					$this->AssertEquals( 1, $p->height());
+					$this->AssertEquals( 41, $p->value());
+					break;
+				case 3:
+					$this->AssertEquals( 1, count($p->contents()));
+					$this->AssertEquals( 10, $p->weight());
+					$this->AssertEquals( 5, $p->width());
+					$this->AssertEquals( 5, $p->length());
+					$this->AssertEquals( 5, $p->height());
+					$this->AssertEquals( 42, $p->value());
+					break;
+				case 4:
+					$this->AssertEquals( 1, count($p->contents()));
+					$this->AssertEquals( 15, $p->weight());
+					$this->AssertEquals( 5, $p->width());
+					$this->AssertEquals( 15, $p->length());
+					$this->AssertEquals( 5, $p->height());
+					$this->AssertEquals( 42, $p->value());
+					break;
+				case 5:
+				case 6:
+				case 7:
+					$this->AssertEquals( 1, count($p->contents()));
+					$this->AssertEquals( 50, $p->weight());
+					$this->AssertEquals( 10, $p->width());
+					$this->AssertEquals( 10, $p->length());
+					$this->AssertEquals( 20, $p->height());
+					$this->AssertEquals( 42, $p->value());
+					break;
+			}
+		}
+	}
 }
 ?>
