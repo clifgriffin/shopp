e78492db727017ed1393f5cce0e099e59b351eb6
Jonathan Davis <jond@ingenesis.net>
2011-09-29 14:48:13 +0000

    Added char 'not null' optimization to the ranges column of the summary table
    git-svn-id: file:///Users/jond/svngit/svnrepos/shopp/trunk@2343 846ab242-8549-0410-9a75-dfdbdae9473e

--- shopp/core/model/schema.sql
+++ shopp/core/model/schema.sql
@@ -23,8 +23,8 @@ CREATE TABLE <?php echo $summary; ?> (							-- Summary table for product state 
 	grossed decimal(16,6) NOT NULL default '0.00',				-- (10) Gross sales
 	maxprice decimal(16,6) NOT NULL default '0.00',				-- (10) Maximum price of all product's price records
 	minprice decimal(16,6) NOT NULL default '0.00',				-- (10) Minimum price of all product's price records
-	ranges char(200),											-- (200) Set of minimum and maximum values
-	taxed set('max price','min price','max saleprice','min saleprice'), -- (2) Which pricetags are taxed
+	ranges char(200) NOT NULL default '',						-- (200) Set of minimum and maximum values
+	taxed set('max price','min price','max saleprice','min saleprice'),	-- (2) Which pricetags are taxed
 	lowstock enum('none','warning','critical','backorder') NOT NULL,	-- (1) Product low stock warning status
 	stock int(10) NOT NULL default '0',							-- (4) Total stock of all product price records
 	inventory enum('off','on') NOT NULL,						-- (1) Product has inventory flag
