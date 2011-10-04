ac1f41d6a81b36af8fe2af6e09c56d07c2a12eb4
Jonathan Davis <jond@ingenesis.net>
2011-10-04 19:07:51 +0000

    Added support for shopp('checkout','clickwrap') theme api tag to allow merchants to specify terms easily, addresses ticket [#1160 state:closed]
    git-svn-id: file:///Users/jond/svngit/svnrepos/shopp/trunk@2360 846ab242-8549-0410-9a75-dfdbdae9473e

--- core/model/Order.php
+++ core/model/Order.php
@@ -801,6 +801,9 @@ class Order {
 		if(apply_filters('shopp_email_valid',!preg_match("!^$rfc822email$!", $_POST['email'])))
 			return new ShoppError(__('You must provide a valid e-mail address.','Shopp'),'cart_validation');
 
+		if (apply_filters(' shopp_clickwrap_required',isset($_POST['data']['clickwrap']) && 'agreed' !== $_POST['data']['clickwrap']) )
+			return new ShoppError(__('You must agree to the terms of sale.','Shopp'),'checkout_validation');
+
 		if ($this->accounts == "wordpress" && !$this->Customer->logged_in()) {
 			require(ABSPATH."/wp-includes/registration.php");
 
