94faa2ff667bde15cd012903ee4ccb428311c806
John Dillick <jdillick@ingenesis.net>
2011-10-24 20:36:09 +0000

    Added unit test for shopp_rmv_cart_item(). Fixed shopp_rmv_cart_item() to update cart totals.
    git-svn-id: file:///Users/jond/svngit/svnrepos/shopp/trunk@2427 846ab242-8549-0410-9a75-dfdbdae9473e

--- t/tests/CartDevAPITests.php
+++ t/tests/CartDevAPITests.php
@@ -47,6 +47,36 @@ class CartDevAPITests extends ShoppTestCase {
 		$this->AssertEquals(91+49.82, $Totals->total);
 	}
 
+	// this test will fail if the above shopp_add_cart_product test does not run
+	function test_shopp_rmv_cart_item () {
+		shopp_rmv_cart_item(1);
+		$items = shopp_cart_items();
+		$this->AssertEquals(1, count($items));
+
+		$item = reset($items);
+
+		$this->AssertEquals('1/10 Carat Diamond Journey Heart Pendant in Yellow Gold', $item->name);
+		$this->AssertEquals('110-carat-diamond-journey-heart-pendant-in-yellow-gold', $item->slug);
+		$this->AssertEquals(2, $item->quantity);
+		$this->AssertEquals(44, $item->unitprice);
+		$this->AssertEquals(88, $item->totald);
+		$this->AssertEquals(88, $item->total);
+
+		$Totals = ShoppOrder()->Cart->Totals;
+
+		$this->AssertEquals(88, $Totals->subtotal);
+		$this->AssertEquals(3, $Totals->shipping);
+		$this->AssertEquals(91, $Totals->total);
+
+		shopp_rmv_cart_item(0);
+		$Totals = ShoppOrder()->Cart->Totals;
+
+		$this->AssertEquals(0, $Totals->subtotal);
+		$this->AssertEquals(false, $Totals->shipping);
+		$this->AssertEquals(0, $Totals->total);
+
+	}
+
 }
 
 ?>
