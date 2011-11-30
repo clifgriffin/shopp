77fa09baed7fd9ef896b289fd78e3fc4ff8f8e4b
Dave Kress <davek@ingenesis.net>
2011-11-30 20:26:48 +0000

    Added pretty url tests for product and category, and default url test for product.
    git-svn-id: file:///Users/jond/svngit/svnrepos/shopp/trunk@2617 846ab242-8549-0410-9a75-dfdbdae9473e

--- t/tests/URLTests.php
+++ t/tests/URLTests.php
@@ -15,10 +15,13 @@
 
 class URLTests extends ShoppTestCase {
 
-	// function setUp () {
-	// 	parent::setUp();
-	// 	ShoppOrder()->Shipping->country = 'US';
-	// }
+	function setUp () {
+		parent::setUp();
+		// ShoppOrder()->Shipping->country = 'US';
+		
+		$Product = shopp_product("Ultimate Matrix Collection", 'name');
+		ShoppProduct($Product);
+	}
 
 	function test_pretty_cart_url () {
 		
@@ -40,6 +43,40 @@ class URLTests extends ShoppTestCase {
 		$this->assertEquals('http://shopptest/store/checkout/',$actual);
 	}
 
+	function test_pretty_account_url () {
+		
+		ob_start();
+		shopp('account','url');
+		$actual = ob_get_contents();
+		ob_end_clean();
+
+		$this->assertEquals('http://shopptest/store/account/',$actual);
+	}
+	
+	function test_pretty_product_url () {
+		
+		ob_start();
+		shopp('product','url');
+		$actual = ob_get_contents();
+		ob_end_clean();
+
+		$this->assertEquals('http://shopptest/store/ultimate-matrix-collection/',$actual);
+	}
+	
+	function test_pretty_category_url () {
+		
+		ob_start();
+		shopp('category','url');
+		$actual = ob_get_contents();
+		ob_end_clean();
+		
+		$this->assertEquals('http://shopptest/store/category/apparel/',$actual);
+	}
+	
+/**
+ * Switch to Default DB Structure
+ **/
+
 	function test_default_urls () {
 		update_option('permalink_structure','');
 		flush_rewrite_rules();
@@ -54,6 +91,37 @@ class URLTests extends ShoppTestCase {
 
 		$this->assertEquals('http://shopptest/?shopp_page=cart',$actual);
 	}
+	
+	function test_default_checkout_url () {
+		
+		ob_start();
+		shopp('checkout','url');
+		$actual = ob_get_contents();
+		ob_end_clean();
+
+		$this->assertEquals('http://shopptest/?shopp_page=checkout',$actual);
+	}
+	
+	function test_default_account_url () {
+		
+		ob_start();
+		shopp('account','url');
+		$actual = ob_get_contents();
+		ob_end_clean();
+
+		$this->assertEquals('http://shopptest/?shopp_page=account',$actual);
+	}
+	
+	function test_default_product_url () {
+		
+		ob_start();
+		shopp('product','url');
+		$actual = ob_get_contents();
+		ob_end_clean();
+
+		$this->assertEquals('http://shopptest/?shopp_product=ultimate-matrix-collection',$actual);
+	}
+	
 	
 } // end URLTests class
 
