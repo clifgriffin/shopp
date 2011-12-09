006aec956c5b124b3e8db3f35df3073e62abac61
Jonathan Davis <jond@ingenesis.net>
2011-12-09 19:23:38 +0000

    Marking current subsubsub in order manager, props brandondove [#1319 state:closed]
    git-svn-id: file:///Users/jond/svngit/svnrepos/shopp/trunk@2664 846ab242-8549-0410-9a75-dfdbdae9473e

--- shopp/core/ui/orders/navigation.php
+++ shopp/core/ui/orders/navigation.php
@@ -1,8 +1,14 @@
 <ul class="subsubsub">
-	<li><a href="<?php echo esc_url(add_query_arg(array_merge($_GET,array('status'=>null,'id'=>null)),admin_url('admin.php'))); ?>"><?php _e('All Orders','Shopp'); ?></a></li>
 	<?php
 		$counts = $this->status_counts();
-		if (!empty($counts)) foreach($counts as $id => $state): ?>
-		<li>| <a href="<?php echo esc_url(add_query_arg(array_merge($_GET,array('status'=>$id,'id'=>null)),admin_url('admin.php'))); ?>"><?php echo $state->label; ?></a> <span class="count">(<?php echo $state->total; ?>)</span></li>
+		foreach((array)$counts as $id => $state):
+			if ('' === $id) $id = null;
+			$status = isset($_GET['status']) && array_key_exists((int)$_GET['status'],$counts)?(int)$_GET['status']:null;
+			$args = array('status'=> $id, 'id'=>null);
+			$url = add_query_arg(array_merge($_GET,$args),admin_url('admin.php'));
+			$classes = $status === $id?' class="current"':'';
+			$separator = is_null($id)?'':'| ';
+	?>
+		<li><?php echo $separator; ?><a href="<?php echo esc_url($url); ?>"<?php echo $classes; ?>><?php esc_html_e($state->label); ?></a> <span class="count">(<?php esc_html_e($state->total); ?>)</span></li>
 	<?php endforeach; ?>
 </ul>
