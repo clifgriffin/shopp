2bd6ddf49029840e86c9e8a4b0f758878f2209e6
Jonathan Davis <jond@ingenesis.net>
2011-12-27 16:44:30 +0000

    Updated the category editor to not display the Arrange Products button until more than 1 product is assigned to the category
    git-svn-id: file:///Users/jond/svngit/svnrepos/shopp/trunk@2781 846ab242-8549-0410-9a75-dfdbdae9473e

--- core/ui/categories/ui.php
+++ core/ui/categories/ui.php
@@ -31,9 +31,11 @@ function settings_meta_box ($Category) {
 <?php _e('Categories, unlike tags, can be or have nested sub-categories.','Shopp'); ?></p>
 
 	<p><input type="hidden" name="spectemplate" value="off" /><input type="checkbox" name="spectemplate" value="on" id="spectemplates-setting" tabindex="11" <?php if (isset($Category->spectemplate) && $Category->spectemplate == "on") echo ' checked="checked"'?> /><label for="spectemplates-setting"> <?php _e('Product Details Template','Shopp'); ?></label><br /><?php _e('Predefined details for products created in this category','Shopp'); ?></p>
-	<p id="facetedmenus-setting"><input type="hidden" name="facetedmenus" value="off" /><input type="checkbox" name="facetedmenus" value="on" id="faceted-setting" tabindex="12" <?php if (isset($Category->facetedmenus) && $Category->facetedmenus == "on") echo ' checked="checked"'?> /><label for="faceted-setting"> <?php _e('Faceted Menus','Shopp'); ?></label><br /><?php _e('Build drill-down filter menus based on the details template of this category','Shopp'); ?></p>
+	<p id="facetedmenus-setting"><input type="hidden" name="facetedmenus" value="off" /><input type="checkbox" name="facetedmenus" value="on" id="faceted-setting" tabindex="12" <?php if (isset($Category->facetedmenus) && $Category->facetedmenus == "on") echo ' checked="checked"'?> /><label for="faceted-setting"> <?php _e('Faceted Menus','Shopp'); ?></label><br /><label><?php _e('Build drill-down filter menus based on the details template of this category','Shopp'); ?></label></p>
 	<p><input type="hidden" name="variations" value="off" /><input type="checkbox" name="variations" value="on" id="variations-setting" tabindex="13"<?php if (isset($Category->variations) && $Category->variations == "on") echo ' checked="checked"'?> /><label for="variations-setting"> <?php _e('Variations','Shopp'); ?></label><br /><?php _e('Predefined selectable product options for products created in this category','Shopp'); ?></p>
+	<?php if ($Category->count > 1): ?>
 	<p><a href="<?php echo add_query_arg(array('page'=>'shopp-categories','id'=>$Category->id,'a'=>'products'),admin_url('admin.php')); ?>" class="button-secondary"><?php _e('Arrange Products','Shopp'); ?></a></p>
+	<?php endif; ?>
 
 	<?php
 }
