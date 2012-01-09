99944e03a541a6da82d2cef870ab307768f6b28c
Jonathan Davis <jond@ingenesis.net>
2012-01-09 20:01:26 +0000

    Updated file import modal to remove the http example if the http URL stream wrapper is not available [#1408 state:closed] Fixed the admin style of the file import modal to prevent line wrapping between the file import input and the attach button
    git-svn-id: file:///Users/jond/svngit/svnrepos/shopp/trunk@2843 846ab242-8549-0410-9a75-dfdbdae9473e

--- shopp/core/ui/products/ui.php
+++ shopp/core/ui/products/ui.php
@@ -345,7 +345,7 @@ function pricing_meta_box ($Product) {
 
 <div id="chooser">
 	<p><label for="import-url"><?php _e('Attach file by URL','Shopp'); ?>&hellip;</label></p>
-	<p><input type="text" name="url" id="import-url" class="fileimport" /><button class="button-secondary" id="attach-file"><small><?php _e('Attach File','Shopp'); ?></small></button><br /><span><label for="import-url">file:///path/to/file.zip, http://server.com/file.zip</label></span></p>
+	<p><input type="text" name="url" id="import-url" class="fileimport" /><button class="button-secondary" id="attach-file"><small><?php _e('Attach File','Shopp'); ?></small></button><br /><span><label for="import-url">file:///path/to/file.zip<?php if (!in_array('http',stream_get_wrappers())): ?>, http://server.com/file.zip<?php endif; ?></label></span></p>
 	<label class="alignleft"><?php _e('Select a file from your computer','Shopp'); ?>:</label>
 	<div class=""><div id="flash-upload-file"></div><button id="ajax-upload-file" class="button-secondary"><small><?php _e('Upload File','Shopp'); ?></small></button></div>
 </div>
