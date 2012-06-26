e7a3532750470b0b3775e0e3926bcbb16b47b04f
Jonathan Davis <jond@ingenesis.net>
2012-06-26 15:36:00 +0000

    Removed inventory cache tracking which causes some installs to break due to  running out of memory
    git-svn-id: file:///Users/jond/svngit/svnrepos/shopp/branches/1.2@3280 846ab242-8549-0410-9a75-dfdbdae9473e

--- core/model/Collection.php
+++ core/model/Collection.php
@@ -81,11 +81,6 @@ class ProductCollection implements Iterator {
 		if ( shopp_setting_enabled('inventory') && ((is_null($nostock) && !shopp_setting_enabled('outofstock_catalog')) || (!is_null($nostock) && !str_true($nostock))) )
 			$where[] = "( s.inventory='off' OR (s.inventory='on' AND s.stock > 0) )";
 
-		// Check for inventory-based queries (for specialized cache support)
-		$wherescan = join('',$where);
-		if (strpos($wherescan,'s.inventory') !== false || strpos($wherescan,'s.stock') !== false)
-			$inventory = true;
-
 		if (str_true($published)) $where[] = "p.post_status='publish'";
 
 		// Sort Order
@@ -199,14 +194,6 @@ class ProductCollection implements Iterator {
 			if ($limited && false === strpos($limit,',')) $cache->total = $this->total = min($limit,$this->total);
 
 			wp_cache_set($cachehash,$cache,'shopp_collection');
-
-			if ($inventory) { // Keep track of inventory-based query caches
-				$caches = shopp_setting('shopp_inventory_collection_caches');
-				if (!is_array($caches)) $caches = array();
-				$caches[] = $cachehash;
-				shopp_set_setting('shopp_inventory_collection_caches',$caches);
-			}
-
 		}
 		if (false === $this->products) $this->products = array();
 
