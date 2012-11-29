d6b8f1ed0168936760cf7174a5fcf274165a709b
Jonathan Davis <jond@ingenesis.net>
2012-11-29 02:13:31 +0000

    Added initial business report framework Added Sales Report Added Products Report
    git-svn-id: file:///Users/jond/svngit/svnrepos/shopp/trunk@3398 846ab242-8549-0410-9a75-dfdbdae9473e

---
+++ core/ui/reports/navigation.php
@@ -0,0 +1,12 @@
+<ul class="subsubsub">
+	<?php
+		foreach((array)$this->reports as $id => $report):
+			$this_report = isset($_GET['report']) && array_key_exists($_GET['report'],$this->reports)?$_GET['report']:'sales';
+			$args = array('report'=> $id);
+			$url = add_query_arg(array_merge($_GET,$args),admin_url('admin.php'));
+			$classes = $this_report === $id?' class="current"':'';
+			$separator = is_null($separator)?'':'| ';
+	?>
+		<li><?php echo $separator; ?><a href="<?php echo esc_url($url); ?>"<?php echo $classes; ?>><?php esc_html_e($this->reports[$id][1]); ?></a></li>
+	<?php endforeach; ?>
+</ul>
