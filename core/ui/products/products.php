e305c0e16973082f104f53ee78bbbee91de12106
WebBaker <barry@freshlybakedwebsites.net>
2013-01-28 14:53:03 -0800

    [#2149] WP3.5 style 'Add New' links: editor and product list

--- core/ui/products/products.php
+++ core/ui/products/products.php
@@ -1,7 +1,7 @@
 <div class="wrap shopp">
 
 	<div class="icon32"></div>
-	<h2><?php _e('Products','Shopp'); ?> <a href="<?php echo esc_url( add_query_arg(array('page'=>$this->Admin->pagename('products'),'id'=>'new'),admin_url('admin.php'))); ?>" class="button add-new"><?php _e('Add New','Shopp'); ?></a></h2>
+	<h2><?php _e('Products','Shopp'); ?> <a href="<?php echo esc_url( add_query_arg(array('page'=>$this->Admin->pagename('products'),'id'=>'new'),admin_url('admin.php'))); ?>" class="add-new-h2"><?php _e('Add New','Shopp'); ?></a> </h2>
 
 	<?php do_action('shopp_admin_notice'); ?>
 
