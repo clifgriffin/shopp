ef24df67b984db805e0225976872a87cc106a22e
barryhughes <barry@codingkills.me>
2013-05-17 10:48:02 -0700

    Fixes issue with nav menu items lacking a type (and so displaying on the front end without an href attribute)

--- core/flow/Admin.php
+++ core/flow/Admin.php
@@ -92,9 +92,10 @@ class ShoppAdmin extends FlowController {
 		add_filter('favorite_actions', array($this, 'favorites'));
 		add_filter('shopp_admin_boxhelp', array($this, 'keystatus'));
 		add_action('load-update.php', array($this, 'admin_css'));
-		add_action('admin_menu',array($this,'taxonomies'),20);
-		add_action('load-nav-menus.php',array($this,'navmenus'));
-		add_action('wp_setup_nav_menu_item',array($this,'navmenu_setup'));
+		add_action('admin_menu',array($this, 'taxonomies'),20);
+		add_action('load-nav-menus.php',array($this, 'navmenus')); // @todo redundant? (Check with JD)
+		add_action('wp_update_nav_menu_item', array($this, 'navmenu_items'));
+		add_action('wp_setup_nav_menu_item',array($this, 'navmenu_setup'));
 
 		add_filter('wp_dropdown_pages', array($this, 'storefront_pages'));
 
@@ -1147,15 +1148,15 @@ class ShoppAdmin extends FlowController {
 				?>
 					<li>
 						<label class="menu-item-title">
-						<input type="checkbox" name="menu-item[<?php echo $_nav_menu_placeholder; ?>][menu-item-shopp-page]" value="<?php echo $pagetype; ?>" class="menu-item-checkbox" /> <?php
+						<input type="checkbox" name="menu-item[<?php esc_html_e( $_nav_menu_placeholder ) ?>][menu-item-shopp-page]" value="<?php esc_attr_e( $pagetype ) ?>" class="menu-item-checkbox" /> <?php
 							echo esc_html( $Page->title() );
 						?></label>
-						<input type="hidden" class="menu-item-db-id" name="menu-item[<?php echo $_nav_menu_placeholder; ?>][menu-item-db-id]" value="0" />
-						<input type="hidden" class="menu-item-object-id" name="menu-item[<?php echo $_nav_menu_placeholder; ?>][menu-item-object-id]" value="<?php echo $name; ?>" />
-						<input type="hidden" class="menu-item-object" name="menu-item[<?php echo $_nav_menu_placeholder; ?>][menu-item-object]" value="<?php echo $name; ?>" />
-						<input type="hidden" class="menu-item-parent-id" name="menu-item[<?php echo $_nav_menu_placeholder; ?>][menu-item-parent-id]" value="0">
-						<input type="hidden" class="menu-item-type" name="menu-item[<?php echo $_nav_menu_placeholder; ?>][menu-item-type]" value="<?php ShoppPages::QUERYVAR; ?>" />
-						<input type="hidden" class="menu-item-title" name="menu-item[<?php echo $_nav_menu_placeholder; ?>][menu-item-title]" value="<?php echo $Page->title(); ?>" />
+						<input type="hidden" class="menu-item-db-id" name="menu-item[<?php esc_attr_e( $_nav_menu_placeholder ) ?>][menu-item-db-id]" value="0" />
+						<input type="hidden" class="menu-item-object-id" name="menu-item[<?php esc_attr_e( $_nav_menu_placeholder ) ?>][menu-item-object-id]" value="<?php esc_attr_e( $name ) ?>" />
+						<input type="hidden" class="menu-item-object" name="menu-item[<?php esc_attr_e( $_nav_menu_placeholder ) ?>][menu-item-object]" value="<?php esc_attr_e( $name ) ?>" />
+						<input type="hidden" class="menu-item-parent-id" name="menu-item[<?php esc_attr_e( $_nav_menu_placeholder ) ?>][menu-item-parent-id]" value="0">
+						<input type="hidden" class="menu-item-type" name="menu-item[<?php esc_attr_e( $_nav_menu_placeholder ) ?>][menu-item-type]" value="<?php esc_attr_e( ShoppPages::QUERYVAR ) ?>" />
+						<input type="hidden" class="menu-item-title" name="menu-item[<?php esc_attr_e( $_nav_menu_placeholder ) ?>][menu-item-title]" value="<?php esc_attr_e( $Page->title() ) ?>" />
 
 					</li>
 				<?php endforeach; ?>
