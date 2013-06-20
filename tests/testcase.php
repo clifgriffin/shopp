0cbdfdf466eb56a6266488f92ca12a29694fcda6
Jonathan Davis <jond@ingenesis.net>
2013-06-20 16:14:26 -0400

    Cleaned up Shopp test case file

--- tests/testcase.php
+++ tests/testcase.php
@@ -7,8 +7,7 @@ class ShoppTestCase extends WP_UnitTestCase {
 
 
 	protected $backupGlobals = FALSE;
-	var $_time_limit = 120; // max time in seconds for a single test function
-	var $shopp_settings = array(); // testing settings, so tests can play nice
+	public $shopp_settings = array(); // testing settings, so tests can play nice
 
 	function setUp() {
 		parent::setUp();
@@ -20,7 +19,6 @@ class ShoppTestCase extends WP_UnitTestCase {
 			restore_error_handler();
 		}
 
-		set_time_limit($this->_time_limit);
 	}
 
 	function tearDown() {
@@ -106,90 +104,3 @@ class ShoppTestCase extends WP_UnitTestCase {
 	}
 
 } // end ShoppTestCase class
-
-
-/*
-function shopp_run_tests($classes, $classname='') {
-	$suite = new PHPUnit_Framework_TestSuite();
-	foreach ($classes as $testcase)
-	if (!$classname or strtolower($testcase) == strtolower($classname)) {
-		$suite->addTestSuite($testcase);
-	}
-
-	#return PHPUnit::run($suite);
-	$result = new PHPUnit_Framework_TestResult;
-	require('PHPUnit/TextUI/ResultPrinter.php');
-	$printer = new PHPUnit_TextUI_ResultPrinter(NULL,true,true);
-	$result->addListener($printer);
-	return array($suite->run($result), $printer);
-}
-
-function get_all_test_cases() {
-	$test_classes = array();
-	$skipped_classes = explode(',',SHOPP_SKIP_TESTS);
-	$all_classes = get_declared_classes();
-	// only classes that extend ShoppTestCase and have names that don't start with _ are included
-	foreach ($all_classes as $class)
-		if ($class{0} != '_' && is_descendent_class('ShoppTestCase', $class) && !in_array($class,$skipped_classes))
-			$test_classes[] = $class;
-	return $test_classes;
-}
-
-function is_descendent_class($parent, $class) {
-
-	$ancestor = strtolower(get_parent_class($class));
-
-	while ($ancestor) {
-		if ($ancestor == strtolower($parent)) return true;
-		$ancestor = strtolower(get_parent_class($ancestor));
-	}
-
-	return false;
-}
-
-function get_shopp_test_files($dir) {
-	$tests = array();
-	$dh = opendir($dir);
-	while (($file = readdir($dh)) !== false) {
-		if ($file{0} == '.') continue;
-		$path = realpath($dir . DIRECTORY_SEPARATOR . $file);
-		$fileparts = pathinfo($file);
-		if (is_file($path) and $fileparts['extension'] == 'php')
-			$tests[] = $path;
-		elseif (is_dir($path))
-			$tests = array_merge($tests, get_shopp_test_files($path));
-	}
-	closedir($dh);
-
-	return $tests;
-}
-
-function shopptests_print_result($printer, $result) {
-	$printer->printResult($result, timer_stop());
-}
-
-// Override error logging to hide stderr messages from being output by PHPUnit
-ini_set('error_log','/dev/null');
-
-// Main Procedures
-require('wp-config.php');
-
-if (!defined('SHOPP_SQL_DATAFILE')) define('SHOPP_SQL_DATAFILE','shopptest.sql');
-system('mysql -u '.DB_USER.' --password='.DB_PASSWORD.' '.DB_NAME.' < '.SHOPP_SQL_DATAFILE);
-
-require(ABSPATH.'wp-settings.php');
-add_action('shopp_init',create_function('','error_reporting(0); ini_set("display_errors",false);'));
-
-if (defined('SHOPP_IMAGES_PATH')) shopp_set_setting('image_path', SHOPP_IMAGES_PATH);
-if (defined('SHOPP_PRODUCTS_PATH')) shopp_set_setting('products_path', SHOPP_PRODUCTS_PATH);
-if (!defined('SHOPP_SKIP_TESTS')) define('SHOPP_SKIP_TESTS','');
-
-define('SHOPP_TESTS_DIR',dirname(__FILE__).'/tests');
-$files = get_shopp_test_files(SHOPP_TESTS_DIR);
-
-foreach ($files as $file) require($file);
-$tests = get_all_test_cases();
-
-list ($result, $printer) = shopp_run_tests($tests);
-shopptests_print_result($printer,$result);
-*/
