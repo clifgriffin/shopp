ab0de58848deebaf0cd417bffbf568d9c360f6b1
Clifton Griffin <clifgriffin@gmail.com>
2013-06-24 15:13:10 -0400

    #2315 Adding jQuery.on() live binding for shipping method updates.

--- core/ui/behaviors/checkout.js
+++ core/ui/behaviors/checkout.js
@@ -148,7 +148,7 @@ jQuery(document).ready(function () {
 
 	}).trigger('change');
 
-	$('.shopp .shipmethod').change(function () {
+	$(document).on('change', '.shopp .shipmethod', function () {
 		if ( $.inArray($('#checkout #shopp-checkout-function').val(), ['process','confirmed']) != -1 ) {
 			var prefix = '.shopp-cart.cart-',
 				spans = 'span'+prefix,
