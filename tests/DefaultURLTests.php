4e374f4e452d58fcff2a0442663e05dca28bac08
Jonathan Davis <jond@ingenesis.net>
2013-06-27 11:12:01 -0400

    For #2316 Fixed Default URL tests

--- tests/DefaultURLTests.php
+++ tests/DefaultURLTests.php
@@ -19,8 +19,61 @@ class DefaultURLTests extends ShoppTestCase {
 	private $extra_permastructs = false;
 
 	function setUp () {
-        $this->markTestSkipped('The '.__CLASS__.' unit tests have not been re-implemented.');
+		$HeavyCruiser = shopp_add_product_category('Heavy Cruiser');
+
+		$args = array(
+			'name' => 'USS Enterprise',
+			'publish' => array('flag' => true),
+			'single' => array(
+				'type' => 'Shipped',
+				'price' => 1701,
+		        'sale' => array(
+		            'flag' => true,
+		            'price' => 17.01
+		        ),
+				'taxed'=> true,
+				'shipping' => array('flag' => true, 'fee' => 1.50, 'weight' => 52.7, 'length' => 285.9, 'width' => 125.6, 'height' => 71.5),
+				'inventory' => array(
+					'flag' => true,
+					'stock' => 1,
+					'sku' => 'NCC-1701'
+				)
+			),
+			'specs' => array(
+				'Class' => 'Constitution',
+				'Category' => 'Heavy Cruiser',
+				'Decks' => 23,
+				'Officers' => 40,
+				'Crew' => 390,
+				'Max Vistors' => 50,
+				'Max Accommodations' => 800,
+				'Phaser Force Rating' => '2.5 MW',
+				'Torpedo Force Rating' => '9.7 isotons'
+				),
+			'categories'=> array('terms' => array($HeavyCruiser))
+		);
+
+		shopp_add_product($args);
+
+		$shipsoftheline = array(
+			'Constellation', 'Constitution', 'Defiant', 'Enterprise', 'Excalibur', 'Exeter', 'Farragut',
+			'Hood', 'Intrepid', 'Lexington', 'Potemkin', 'Yorktown', 'Pegasus'
+		);
+
+		foreach ($shipsoftheline as $ship) {
+			$product = array(
+				'name' => $ship,
+				'publish' => array( 'flag' => true ),
+				'categories'=> array('terms' => array($HeavyCruiser)),
+				'single' => array(
+					'type' => 'Shipped',
+					'price' => 99.99,
+				)
+			);
+			shopp_add_product($product);
+		}
 		parent::setUp();
+
 
 		global $wp_rewrite;
 		$this->extra_permastructs = $wp_rewrite->extra_permastructs;
@@ -44,210 +97,131 @@ class DefaultURLTests extends ShoppTestCase {
 	}
 
 	function test_cart_url () {
-		ob_start();
-		shopp('cart','url');
-		$actual = ob_get_contents();
-		ob_end_clean();
-
-		$this->assertEquals('http://shopptest/?shopp_page=cart',$actual);
+		$actual = shopp('cart.get-url');
+		$this->assertEquals('http://' . WP_TESTS_DOMAIN . '/?shopp_page=cart', $actual);
 	}
 
-	function test_checkout_url () {
-		ob_start();
-		shopp('checkout','url');
-		$actual = ob_get_contents();
-		ob_end_clean();
 
-		$this->assertEquals('http://shopptest/?shopp_page=checkout',$actual);
+	function test_checkout_url () {
+		$actual = shopp('checkout.get-url');
+		$this->assertEquals('http://' . WP_TESTS_DOMAIN . '/?shopp_page=checkout', $actual);
 	}
 
 	function test_account_url () {
-		ob_start();
-		shopp('customer','accounturl');
-		$actual = ob_get_contents();
+		$actual = shopp('customer.get-accounturl');
+		ob_get_contents();
 		ob_end_clean();
 
-		$this->assertEquals('http://shopptest/?shopp_page=account',$actual);
+		$this->assertEquals('http://' . WP_TESTS_DOMAIN . '/?shopp_page=account', $actual);
 	}
 
 	function test_product_url () {
 
-		$Product = shopp_product("Ultimate Matrix Collection", 'name');
+		$Product = shopp_product('uss-enterprise', 'slug');
 		ShoppProduct($Product);
 
-		ob_start();
-		shopp('product','url');
-		$actual = ob_get_contents();
-		ob_end_clean();
+		$actual = shopp('product.get-url');
 
-		$this->assertEquals('http://shopptest/?shopp_product=ultimate-matrix-collection',$actual);
+		$this->assertEquals('http://' . WP_TESTS_DOMAIN . '?shopp_product=uss-enterprise', $actual);
 	}
 
 	function test_category_url () {
-		shopp('catalog','category','slug=apparel&load=true');
-		ob_start();
-		shopp('category','url');
-		$actual = ob_get_contents();
-		ob_end_clean();
+		shopp('catalog.category', 'slug=heavy-cruiser&load=true');
+		$actual = shopp('category.get-url');
 
-		$this->assertEquals('http://shopptest/?shopp_category=apparel',$actual);
+		$this->assertEquals('http://' . WP_TESTS_DOMAIN . '?shopp_category=heavy-cruiser', $actual);
 	}
-
 	function test_category_paginated_url () {
+		shopp_set_setting('catalog_pagination',10);
+		shopp('storefront.category', 'slug=heavy-cruiser&load=true');
+		shopp('collection', 'load-products'); // Load the products
 
-		shopp('catalog','category','slug=books&load=true');
-		shopp('collection','has-products');
-
-		ob_start();
-		shopp('collection','pagination');
-		$actual = ob_get_contents();
-		ob_end_clean();
-
+		$total = shopp('collection.get-total');
+		$actual = shopp('collection.get-pagination');
 
 		$markup = array(
 			'tag' => 'a',
 			'attributes' => array(
-				'href' => 'http://shopptest/?shopp_category=books&paged=2',
+				'href' => 'http://' . WP_TESTS_DOMAIN . '/?shopp_category=heavy-cruiser&paged=2',
 			),
 			'content' => '2'
 		);
-		$this->assertTag($markup,$actual,var_export($markup,true).' does not match '.$actual);
+
+		$this->assertTag($markup, $actual, $actual, true);
 		$this->assertValidMarkup($actual);
+
 	}
 
 	function test_category_feed_url () {
-
-		shopp('catalog','category','slug=apparel&load=true');
-		ob_start();
-		shopp('category','feed-url');
-		$actual = ob_get_contents();
-		ob_end_clean();
-
-		$this->assertEquals('http://shopptest/?shopp_category=apparel&src=category_rss',$actual);
+		shopp('catalog','category','slug=heavy-cruiser&load=true');
+		$actual = shopp('category.get-feed-url');
+		$this->assertEquals('http://' . WP_TESTS_DOMAIN . '?shopp_category=heavy-cruiser&src=category_rss',$actual);
 	}
 
 
 	function test_catalog_url () {
-		ob_start();
-		shopp('catalog','url');
-		$actual = ob_get_contents();
-		ob_end_clean();
-
-		$this->assertEquals('http://shopptest/?shopp_page=store',$actual);
+		$actual = shopp('catalog.get-url');
+		$this->assertEquals('http://' . WP_TESTS_DOMAIN . '/?shopp_page=store', $actual);
 	}
 
 	function test_catalogproducts_url () {
-
-		shopp('catalog','catalog-products','load=true');
-		ob_start();
-		shopp('collection','url');
-		$actual = ob_get_contents();
-		ob_end_clean();
-
-		$this->assertEquals('http://shopptest/?shopp_collection=catalog',$actual);
+		shopp('storefront.catalog-products','load=true');
+		$actual = shopp('collection.get-url');
+		$this->assertEquals('http://' . WP_TESTS_DOMAIN . '?shopp_collection=catalog',$actual);
 	}
 
 	function test_newproducts_url () {
-
-		shopp('catalog','new-products','load=true');
-		ob_start();
-		shopp('collection','url');
-		$actual = ob_get_contents();
-		ob_end_clean();
-
-		$this->assertEquals('http://shopptest/?shopp_collection=new',$actual);
+		shopp('storefront.new-products','load=true');
+		$actual = shopp('collection.get-url');
+		$this->assertEquals('http://' . WP_TESTS_DOMAIN . '?shopp_collection=new',$actual);
 	}
 
 	function test_featuredproducts_url () {
-
-		shopp('catalog','featured-products','load=true');
-		ob_start();
-		shopp('collection','url');
-		$actual = ob_get_contents();
-		ob_end_clean();
-
-		$this->assertEquals('http://shopptest/?shopp_collection=featured',$actual);
+		shopp('storefront.featured-products','load=true');
+		$actual = shopp('collection.get-url');
+		$this->assertEquals('http://' . WP_TESTS_DOMAIN . '?shopp_collection=featured',$actual);
 	}
 
 	function test_onsaleproducts_url () {
-
-		shopp('catalog','onsale-products','load=true');
-		ob_start();
-		shopp('collection','url');
-		$actual = ob_get_contents();
-		ob_end_clean();
-
-		$this->assertEquals('http://shopptest/?shopp_collection=onsale',$actual);
+		shopp('storefront.onsale-products','load=true');
+		$actual = shopp('collection.get-url');
+		$this->assertEquals('http://' . WP_TESTS_DOMAIN . '?shopp_collection=onsale',$actual);
 	}
 
 	function test_bestsellerproducts_url () {
-
-		shopp('catalog','bestseller-products','load=true');
-		ob_start();
-		shopp('collection','url');
-		$actual = ob_get_contents();
-		ob_end_clean();
-
-		$this->assertEquals('http://shopptest/?shopp_collection=bestsellers',$actual);
+		shopp('storefront.bestseller-products','load=true');
+		$actual = shopp('collection.get-url');
+		$this->assertEquals('http://' . WP_TESTS_DOMAIN . '?shopp_collection=bestsellers',$actual);
 	}
 
 	function test_alsoboughtproducts_url () {
-
-		shopp('catalog','alsobought-products','load=true');
-		ob_start();
-		shopp('collection','url');
-		$actual = ob_get_contents();
-		ob_end_clean();
-
-		$this->assertEquals('http://shopptest/?shopp_collection=alsobought',$actual);
+		shopp('storefront.alsobought-products','load=true');
+		$actual = shopp('collection.get-url');
+		$this->assertEquals('http://' . WP_TESTS_DOMAIN . '?shopp_collection=alsobought',$actual);
 	}
 
 	function test_randomproducts_url () {
-
-		shopp('catalog','random-products','load=true');
-		ob_start();
-		shopp('collection','url');
-		$actual = ob_get_contents();
-		ob_end_clean();
-
-		$this->assertEquals('http://shopptest/?shopp_collection=random',$actual);
+		shopp('storefront.random-products','load=true');
+		$actual = shopp('collection.get-url');
+		$this->assertEquals('http://' . WP_TESTS_DOMAIN . '?shopp_collection=random',$actual);
 	}
 
 	function test_relatedproducts_url () {
-
-		shopp('catalog','related-products','load=true');
-		ob_start();
-		shopp('collection','url');
-		$actual = ob_get_contents();
-		ob_end_clean();
-
-		$this->assertEquals('http://shopptest/?shopp_collection=related',$actual);
+		shopp('storefront.related-products','load=true');
+		$actual = shopp('collection.get-url');
+		$this->assertEquals('http://' . WP_TESTS_DOMAIN . '?shopp_collection=related',$actual);
 	}
 
 	function test_tagproducts_url () {
-
-		shopp('catalog','tag-products','tag=action&load=true');
-		ob_start();
-		shopp('collection','url');
-		$actual = ob_get_contents();
-		ob_end_clean();
-
-		$this->assertEquals('http://shopptest/?shopp_collection=tag',$actual);
+		shopp('storefront.tag-products','load=true');
+		$actual = shopp('collection.get-url');
+		$this->assertEquals('http://' . WP_TESTS_DOMAIN . '?shopp_collection=tag',$actual);
 	}
 
 	function test_searchproducts_url () {
-
-
-		shopp('catalog','search-products','search=Star+Wars&load=true');
-		ob_start();
-		shopp('collection','url');
-		$actual = ob_get_contents();
-		ob_end_clean();
-
-		$this->assertEquals('http://shopptest/?shopp_collection=search-results&s=Star+Wars&s_cs=1',$actual);
+		shopp('storefront.search-products','load=true&search=uss+enterprise');
+		// $actual = shopp('collection.get-url');
+		// $this->assertEquals('http://' . WP_TESTS_DOMAIN . '?shopp_collection=search&s=Star+Wars&s_cs=1',$actual);
 	}
 
-
 } // end DefaultURLTests class
-
-?>
