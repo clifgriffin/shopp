b0bf365f1f1b880246effc344a68c3f8e38b9a02
Jonathan Davis <jond@ingenesis.net>
2013-07-08 15:22:44 -0400

    Fixed #2339 Determine free shipping for the product based on a Shipped price tags with no shipping settings

--- core/model/Product.php
+++ core/model/Product.php
@@ -560,7 +560,7 @@ class Product extends WPShoppObject {
 		}
 
 		$freeshipping = false;
-		if (!str_true($price->shipping)) $freeshipping = true;
+		if ( ! Shopp::str_true($price->shipping) && 'Shipped' == $price->type ) $freeshipping = true;
 
 		// Calculate catalog discounts if not already calculated
 		if (!empty($price->discounts)) {
