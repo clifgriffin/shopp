6ccff5c503499393c4506c1af8afbcc5d6fbbd5b
barryhughes <barry@codingkills.me>
2013-07-23 16:50:34 -0700

    Cleanup code for test_translate(): removes the l10n filters after first use as they will not subsequently be required.

--- tests/CoreTests.php
+++ tests/CoreTests.php
@@ -21,7 +21,6 @@ class CoreTests extends ShoppTestCase {
 		$translation = Shopp::translate('Some of the colonists objected to having an anatomically correct android running around without any clothes on');
 		$this->assertTrue($translation === self::TRANSLATED);
 		$this->assertTrue($this->domain === 'ShoppCore');
-
 		$this->assertEmpty($this->context);
 
 		$translation = Shopp::translate("Who knows if we're even dead or alive?", "Geordi La Forge's philosophy");
@@ -36,11 +35,13 @@ class CoreTests extends ShoppTestCase {
 	}
 
 	public function filter_gettext($translation, $text, $domain) {
+		remove_filter('gettext', array($this, 'filter_gettext'), 10, 3);
 		$this->domain = $domain;
 		return self::TRANSLATED;
 	}
 
 	public function filter_gettext_with_context($translation, $text, $context, $domain) {
+		remove_filter('gettext_with_context', array($this, 'filter_gettext_with_context'), 10, 4);
 		$this->domain = $domain;
 		$this->context = $context;
 		return self::TRANSLATED;
