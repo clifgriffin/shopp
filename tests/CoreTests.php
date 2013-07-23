16bf16f2e2af070c04969f5c4b23cb1cfa466d0f
barryhughes <barry@codingkills.me>
2013-07-23 16:49:21 -0700

    Test for Shopp::translate() added

--- tests/CoreTests.php
+++ tests/CoreTests.php
@@ -5,7 +5,44 @@
  * @copyright Ingenesis Limited, 23 July 2013
  */
 class CoreTests extends ShoppTestCase {
+	const TRANSLATED = 'Translated!';
+
+	public $domain = '';
+	public $context = '';
+
+
 	public function test_unsupported () {
 		$this->assertTrue(defined('SHOPP_UNSUPPORTED'));
+	}
+
+	public function test_translate() {
+		$this->setup_translation_filters();
+
+		$translation = Shopp::translate('Some of the colonists objected to having an anatomically correct android running around without any clothes on');
+		$this->assertTrue($translation === self::TRANSLATED);
+		$this->assertTrue($this->domain === 'ShoppCore');
+
+		$this->assertEmpty($this->context);
+
+		$translation = Shopp::translate("Who knows if we're even dead or alive?", "Geordi La Forge's philosophy");
+		$this->assertTrue($translation === self::TRANSLATED);
+		$this->assertTrue($this->context === "Geordi La Forge's philosophy");
+		$this->assertTrue($this->domain === 'ShoppCore');
+	}
+
+	protected function setup_translation_filters() {
+		add_filter('gettext', array($this, 'filter_gettext'), 10, 3);
+		add_filter('gettext_with_context', array($this, 'filter_gettext_with_context'), 10, 4);
+	}
+
+	public function filter_gettext($translation, $text, $domain) {
+		$this->domain = $domain;
+		return self::TRANSLATED;
+	}
+
+	public function filter_gettext_with_context($translation, $text, $context, $domain) {
+		$this->domain = $domain;
+		$this->context = $context;
+		return self::TRANSLATED;
 	}
 }
