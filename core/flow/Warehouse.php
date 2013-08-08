84186dc8d21411c2f89c0817d97d55e35e59f8a8
Jonathan Davis <jond@ingenesis.net>
2013-08-08 11:54:05 -0400

    Turned off query debugging for the inventory view query

--- core/flow/Warehouse.php
+++ core/flow/Warehouse.php
@@ -396,7 +396,7 @@ class Warehouse extends AdminController {
 				'limit'=>"$start,$per_page",
 				'nostock' => true,
 				'published' => false,
-				'debug' => true
+				// 'debug' => true
 			);
 		}
 
