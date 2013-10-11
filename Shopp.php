2f56b9bf39cadf35ddff5d20dbfe2c20dd31e691
Jonathan Davis <jond@ingenesis.net>
2013-10-11 17:01:23 -0400

    1.3 Release Candidate 2

--- Shopp.php
+++ Shopp.php
@@ -3,7 +3,7 @@
  * Plugin Name: Shopp
  * Plugin URI: http://shopplugin.com
  * Description: An ecommerce framework for WordPress.
- * Version: 1.3RC1
+ * Version: 1.3RC2
  * Author: Ingenesis Limited
  * Author URI: http://ingenesis.net
  * Requires at least: 3.5
