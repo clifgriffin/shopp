a62567f837537972f02ee763842ade6678e31da5
Jonathan Davis <jond@ingenesis.net>
2013-10-17 16:20:26 -0400

    Moved labels above fields to mimic checkout template layout

--- templates/login.php
+++ templates/login.php
@@ -12,17 +12,17 @@
 ?>
 
 <?php if ( shopp( 'customer.notloggedin' ) ) : ?>
-	<form action="<?php shopp( 'customer.url' ); ?>" method="post" class="shopp" id="login">
+	<form action="<?php shopp( 'customer.url' ); ?>" method="post" class="shopp shopp_page" id="login">
 		<ul>
 			<li>
 				<label for="login"><?php _e( 'Account Login', 'Shopp' ); ?></label>
 				<span>
-					<?php shopp( 'customer.account-login', 'size=20&title=' . __( 'Login', 'Shopp' ) ); ?>
 					<label for="login"><?php shopp( 'customer.login-label' ); ?></label>
+					<?php shopp( 'customer.account-login', 'size=20&title=' . __( 'Login', 'Shopp' ) ); ?>
 				</span>
 				<span>
-					<?php shopp( 'customer.password-login', 'size=20&title=' . __( 'Password', 'Shopp' ) ); ?>
 					<label for="password"><?php _e( 'Password', 'Shopp' ); ?></label>
+					<?php shopp( 'customer.password-login', 'size=20&title=' . __( 'Password', 'Shopp' ) ); ?>
 				</span>
 				<span><?php shopp( 'customer.login-button' ); ?></span>
 			</li>
