clifgriffin/shopp

commit b2728046df95059b75e9227bb471252e1534b530

Fixed #2660 Reuse ShoppCartItem->load() to change selected price variant to maintain Item instance and update prices

core/model/Cart.php
@@ -485,17 +485,13 @@ class ShoppCart extends ListFramework {
 
 		// Maintain item state, change variant
 		$Item = $this->get($item);
-		$qty = $Item->quantity;
 		$category = $Item->category;
 		$data = $Item->data;
 
-		foreach ($Item->addons as $addon)
+		foreach ( $Item->addons as $addon )
 			$addons[] = $addon->options;
 
-		$UpdatedItem = new ShoppCartItem(new ShoppProduct($product), $pricing, $category, $data, $addons);
-		$UpdatedItem->quantity($qty);
-
-		parent::update($item, $UpdatedItem);
+		$Item->load(new ShoppProduct($product), $pricing, $category, $data, $addons);
 
 		return true;
 	}

core/model/Item.php
@@ -154,14 +154,14 @@ class ShoppCartItem {
 		$this->description = $Product->summary;
 
 		// Product has variants
-		if ( Shopp::str_true($Product->variants) )
+		if ( Shopp::str_true($Product->variants) && empty($this->variants) )
 			$this->variants($Product->prices);
 
 		// Product has Addons
-		if ( Shopp::str_true($Product->addons) )
+		if ( Shopp::str_true($Product->addons) && empty($this->addons) )
 			$this->addons($this->addonsum, $addons, $Product->prices);
 
-		if (isset($Price->id))
+		if ( isset($Price->id) )
 			$this->option = $this->mapprice($Price);
 
 		$this->sku = $Price->sku;
@@ -173,6 +173,7 @@ class ShoppCartItem {
 		$this->unitprice = $baseprice + $this->addonsum;
 
 		if ( shopp_setting_enabled('taxes') ) {
+			$this->taxable = array();
 			if ( Shopp::str_true($Price->tax) ) $this->taxable[] = $baseprice;
 			$this->istaxed = ( $this->taxable > 0 );
 			$this->includetax = shopp_setting_enabled('tax_inclusive');

core/library/Loader.php
@@ -209,7 +209,7 @@ class ShoppLoader {
 		fclose($file);
 	}
 
-	static function find_wpload () {
+	public static function find_wpload () {
 
 		$configfile = 'wp-config.php';
 		$loadfile = 'wp-load.php';

commit 7d3e9bc01515cfeb3c11ccd8138f58bfab046531

Fixed shopp_rmv_cart_item_addon() broken due to changes in b272804

api/cart.php
@@ -342,17 +342,16 @@ function shopp_rmv_cart_item_addon ( $itemkey = false, $addonkey = false ) {
 		return false; // Debug message will already have been generated in shopp_cart_item_addons()
 	}
 
-	foreach ($addons as $existing)
-		if ( $existing->id == $addonkey ) $exists = true;
-
-	if ( ! $exists ) {
+	if ( ! isset($addons[ $addonkey ]) ) {
 		shopp_debug(__FUNCTION__ . " failed: addon $addonkey was not found in item $itemkey");
 		return false;
 	}
 
 	$revised = array();
-	foreach ( $addons as $addon )
-		if ( $addonkey != $addon->id ) $revised[] = $addon->id;
+	foreach ( $addons as $addon ) {
+		if ( $addonkey == $addon->id ) $revised[] = (int)($addonkey * -1);
+		else $revised[] = $addon->id;
+	}
 
 	return $Order->Cart->change($itemkey, $item->product, (int) $item->priceline, $revised);
 }

core/model/Cart.php
@@ -490,9 +490,6 @@ class ShoppCart extends ListFramework {
 		$category = $Item->category;
 		$data = $Item->data;
 
-		foreach ( $Item->addons as $addon )
-			$addons[] = $addon->options;
-
 		$Item->load(new ShoppProduct($product), $pricing, $category, $data, $addons);
 		ShoppOrder()->Shiprates->item( new ShoppShippableItem($Item) );
 

core/model/Item.php
@@ -158,8 +158,11 @@ class ShoppCartItem {
 			$this->variants($Product->prices);
 
 		// Product has Addons
-		if ( Shopp::str_true($Product->addons) && empty($this->addons) )
+		if ( Shopp::str_true($Product->addons) ) {
+			if ( ! empty($this->addons) ) // Compute addon differences
+				$addons = array_diff($addons, array_keys($this->addons));
 			$this->addons($this->addonsum, $addons, $Product->prices);
+		}
 
 		if ( isset($Price->id) )
 			$this->option = $this->mapprice($Price);
@@ -435,31 +438,42 @@ class ShoppCartItem {
 	 * @param array $prices A list of Price objects
 	 * @return void
 	 **/
-	public function addons (&$sum,$addons,$prices,$property='pricing') {
-		foreach ($prices as $p)	{
-			if ('N/A' == $p->type || 'addon' != $p->context) continue;
+	public function addons ( &$sum, array $addons, array $prices, $property = 'pricing' ) {
+
+		foreach ( $prices as $p )	{
+			if ( 'N/A' == $p->type || 'addon' != $p->context ) continue;
 			$pricing = $this->mapprice($p);
-			if (empty($pricing) || !in_array($pricing->id,$addons)) continue;
-			if ('Shipped' == $p->type) $this->shipped = true;
-			if ($property == 'pricing') {
-				$pricing->unitprice = (Shopp::str_true($p->sale)?$p->promoprice:$p->price);
-				$this->addons[] = $pricing;
+
+			if ( empty($pricing) ) continue; // Skip if no pricing available
+			if ( in_array( ($pricing->id * -1), $addons ) ) { // If the addon is marked for removal
+				unset($this->addons[ $pricing->id ]); // Remove it from our addons list
+				continue;
+			}
+
+			if ( ! in_array($pricing->id, $addons)) continue;
+
+			if ( 'Shipped' == $p->type ) $this->shipped = true;
+
+			if ( 'pricing' == $property ) {
+				$pricing->unitprice = Shopp::str_true($p->sale) ? $p->promoprice : $p->price;
+				$this->addons[ $pricing->id ] = $pricing;
 				$sum += $pricing->unitprice;
 
-				if (shopp_setting_enabled('taxes') && Shopp::str_true($pricing->tax))
+				if ( shopp_setting_enabled('taxes') && Shopp::str_true($pricing->tax) )
 					$this->taxable[] = $pricing->unitprice;
 
-			} elseif ('dimensions' == $property) {
+			} elseif ( 'dimensions' == $property ) {
 				if ( ! Shopp::str_true($p->shipping) || 'Shipped' != $p->type ) continue;
-				foreach ($p->dimensions as $dimension => $value)
-					$sum[$dimension] += $value;
-			} elseif ('shipfee' == $property) {
+				foreach ( $p->dimensions as $dimension => $value )
+					$sum[ $dimension ] += $value;
+			} elseif ( 'shipfee' == $property ) {
 				if ( ! Shopp::str_true($p->shipping) ) continue;
 				$sum += $pricing->shipfee;
 			} else {
-				if (isset($pricing->$property)) $sum += $pricing->$property;
+				if ( isset($pricing->$property) ) $sum += $pricing->$property;
 			}
 		}
+
 	}
 
 	/**

tests/CartDevAPITests.php
@@ -297,7 +297,7 @@ class CartDevAPITests extends ShoppTestCase {
 	}
 
 	function test_shopp_rmv_cart_addon() {
-		$this->markTestSkipped('Skipped.');
+		// $this->markTestSkipped('Skipped.');
 		$Product = shopp_product('galileo', 'slug');
 		shopp_add_cart_product($Product->id, 1);
 
@@ -319,7 +319,7 @@ class CartDevAPITests extends ShoppTestCase {
 	}
 
 	function test_shopp_cart_item_addons() {
-		$this->markTestSkipped('Skipped.');
+		// $this->markTestSkipped('Skipped.');
 		$Product = shopp_product('galileo', 'slug');
 		shopp_add_cart_product($Product->id, 1);
 
@@ -344,7 +344,7 @@ class CartDevAPITests extends ShoppTestCase {
 	 * @depends test_shopp_cart_item_addons
 	 */
 	function test_shopp_cart_item_addons_count() {
-		$this->markTestSkipped('Skipped.');
+		// $this->markTestSkipped('Skipped.');
 		$Product = shopp_product('galileo', 'slug');
 		shopp_add_cart_product($Product->id, 1);
 