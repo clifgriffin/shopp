667ced90c06abebe3a93653862fb430c4bbfec3f
Jonathan Davis <jond@ingenesis.net>
2014-02-26 16:56:19 -0500

    Version bump to 1.3.2 for release

--- core/library/Version.php
+++ core/library/Version.php
@@ -24,7 +24,7 @@ class ShoppVersion {
 	const PATCH = 2;
 
 	/** @type string PRERELEASE The prerelease designation (dev, beta, RC1) */
-	const PRERELEASE = 'dev';
+	const PRERELEASE = '';
 
 	/** @type string CODENAME The release project code name */
 	const CODENAME = 'Phobos';
