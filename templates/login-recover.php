5f70fc4561be68de0e0fb691910123eef777f75c
Jonathan Davis <jond@ingenesis.net>
2014-03-25 16:51:11 -0400

    Fixed #2839 Corrected warning header formatting so theme template copying works

--- templates/login-recover.php
+++ templates/login-recover.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * WARNING! DO NOT EDIT!
- *
- * These templates are part of the core Shopp files
- * and will be overwritten when upgrading Shopp.
- *
- * For editable templates, setup Shopp theme templates:
- * http://docs.shopplugin.net/Setting_Up_Theme_Templates
- *
-*/
+ ** WARNING! DO NOT EDIT!
+ **
+ ** These templates are part of the core Shopp files
+ ** and will be overwritten when upgrading Shopp.
+ **
+ ** For editable templates, setup Shopp theme templates:
+ ** http://shopplugin.com/docs/the-catalog/theme-templates/
+ **
+ **/
 ?>
 
 <form action="<?php shopp( 'customer', 'url' ); ?>" method="post" class="shopp" id="login">
