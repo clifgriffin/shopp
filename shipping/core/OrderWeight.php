da581de12f879d7a8e7af332076ed19f72af96a1
Clifton Griffin <clifgriffin@gmail.com>
2014-05-09 12:31:48 -0400

    Change for all tiered shipping modules: If no tiers match (for lowest tier), do not return a rate.

--- shipping/core/OrderWeight.php
+++ shipping/core/OrderWeight.php
@@ -38,12 +38,20 @@ class OrderWeight extends ShippingFramework implements ShippingModule {
 			if ( false === $tiers ) continue; // Skip methods that don't match at all
 
 			$amount = 0;
+			$matched = false;
 			$tiers = array_reverse($tiers);
+			
 			foreach ( $tiers as $tier ) {
 				extract($tier);
 				$amount = Shopp::floatval($rate);			// Capture the rate amount
-				if ( $this->weight >= $threshold ) break;
+				
+				if ( $this->weight >= $threshold ) {
+					$matched = true;
+					break;	
+				} 
 			}
+			
+			if ( ! $matched ) return $options;
 
 			$rate = array(
 				'slug' => $slug,
