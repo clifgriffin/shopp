56c832d9aa5af7e095340c01ff46d6c8f2232c0f
Clifton Griffin <clifgriffin@gmail.com>
2014-06-02 15:32:39 -0400

    Fix typo in fb81faf57d0a0f37c952ecaebac5b15154270237

--- core/model/Shiprates.php
+++ core/model/Shiprates.php
@@ -218,7 +218,7 @@ class ShoppShiprates extends ListFramework {
 
 		do_action('shopp_calculate_shipping_init');		// Initialize shipping modules
 
-		parent:clear();									// clear existing rates before we pull new ones
+		parent::clear();									// clear existing rates before we pull new ones
 		
 		$this->items();									// Send items to shipping modules that package them
 
