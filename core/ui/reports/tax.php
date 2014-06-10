61f1afbafd0d05f63d4e45f77040296a410da1e5
Jonathan Davis <jond@ingenesis.net>
2014-06-10 12:11:44 -0400

    Fixed #2960 Corrected subqueries for proper record aggregation

--- core/ui/reports/tax.php
+++ core/ui/reports/tax.php
@@ -31,8 +31,8 @@ class TaxReport extends ShoppReportFramework implements ShoppReport {
 							UNIX_TIMESTAMP(o.created) as period,
 							COUNT(DISTINCT o.id) AS orders,
 							SUM(o.subtotal) as subtotal,
-							SUM( (SELECT IF(p.unittax > 0,p.total,0) FROM $purchased_table AS p WHERE o.id = p.purchase) ) AS taxable,
-							AVG( (SELECT p.unittax/p.unitprice FROM $purchased_table AS p WHERE o.id = p.purchase) ) AS rate,
+							( SELECT SUM(IF(p.unittax > 0,p.total,0)) FROM $purchased_table AS p WHERE o.id = p.purchase ) AS taxable,
+							( SELECT AVG(p.unittax/p.unitprice) FROM $purchased_table AS p WHERE o.id = p.purchase ) AS rate,
 							SUM(o.tax) as tax
 					FROM $orders_table AS o
 					WHERE $where
