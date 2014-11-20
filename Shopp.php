ac35437c11576dd5cbddf7a54727bc8109e215f1
Jonathan Davis <jond@ingenesis.net>
2014-11-20 15:57:33 -0500

    Version change to 1.4dev

--- Shopp.php
+++ Shopp.php
@@ -3,7 +3,7 @@
  * Plugin Name: Shopp
  * Plugin URI: http://shopplugin.com
  * Description: An ecommerce framework for WordPress.
- * Version: 1.4.0dev
+ * Version: 1.4dev
  * Author: Ingenesis Limited
  * Author URI: http://ingenesis.net
  * Requires at least: 3.5
