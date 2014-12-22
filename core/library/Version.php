d70e4e0f6270260416cb78f055529eb3e8d29200
Jonathan Davis <jond@ingenesis.net>
2014-12-22 11:42:03 -0500

    Project Cydonia begins for 1.3.9dev

--- core/library/Version.php
+++ core/library/Version.php
@@ -21,13 +21,13 @@ class ShoppVersion {
 	const MINOR = 3;
 
 	/** @type int PATCH The maintenance patch version number */
-	const PATCH = 8;
+	const PATCH = 9;
 
 	/** @type string PRERELEASE The prerelease designation (dev, beta, RC1) */
-	const PRERELEASE = '';
+	const PRERELEASE = 'dev';
 
 	/** @type string CODENAME The release project code name */
-	const CODENAME = 'Barsoom';
+	const CODENAME = 'Cydonia';
 
 	/** @type int DB The database schema version */
 	const DB = 1201;
