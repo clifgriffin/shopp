62ab7c066acd07341904863d42311b3f93f63a3b
Jonathan Davis <jond@ingenesis.net>
2015-01-27 10:09:38 -0500

    Fixed #3203 Ensure the order lock is destroyed during shutdown
    If execution of PHP suddenly halts (PHP Fatal error, for instance) it will leave the lock in place, blocking other processes from being able to manipulate the order in the session. To prevent this, a handler is added to delete the transient during the WP shutdown action, which fires whenever the PHP process is ending. This ensures locks are always cleared after the PHP processing ends.

--- core/flow/Order.php
+++ core/flow/Order.php
@@ -272,7 +272,10 @@ class ShoppOrder {
 			Shopp::redirect(Shopp::url(array('inprogress' => '1'), 'checkout', $this->security()));
 			return;
 
-		} else set_transient($lockid, true, $timeout);
+		} else {
+			set_transient($lockid, true, $timeout);
+			add_action('shutdown', create_function('', 'delete_transient("' . $lockid . '");'));
+		}
 
 		shopp_add_order_event(false, 'purchase', array(
 			'gateway' => $this->Payments->processor()
