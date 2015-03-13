fa6406928358239f8d06ac437e7c30f30993e5fc
Marcel M. Cary <marcel@oak.homeunix.org>
2015-03-13 05:58:41 -0700

    Restore "Charge Order" button for orders placed before Shopp 1.3.x
    The "Charge Order" button is missing from the Order detail admin page in
    Shopp 1.3.x for orders placed on 1.2.5.  The reason is that the class
    names of the gateways have changed to include a "Shopp" prefix, so the
    Gateway lookup fails.
    
    If a gateway's name is not found as stored with the Purchase record, try
    to look it up again with the "Shopp" prefix.
    
    https://github.com/ingenesis/shopp/issues/3253

--- core/model/Purchase.php
+++ core/model/Purchase.php
@@ -301,6 +301,9 @@ class ShoppPurchase extends ShoppDatabaseObject {
 		$processor = $this->gateway;
 		if ( 'ShoppFreeOrder' == $processor ) return $Gateways->freeorder;
 		if ( isset($Gateways->active[ $processor ]) ) return $Gateways->active[ $processor ];
+		// Work around gateway renaming in 1.3.x:
+		// https://github.com/ingenesis/shopp/issues/3253
+		if ( isset($Gateways->active[ 'Shopp'.$processor ]) ) return $Gateways->active[ 'Shopp'.$processor ];
 		else {
 			foreach ( $Gateways->active as $Gateway ) {
 				if ($processor != $Gateway->name) continue;
