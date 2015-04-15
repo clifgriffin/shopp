14f8ae96358e75ac22262111b0dca32d6457b9ff
Jonathan Davis <jond@ingenesis.net>
2015-04-15 11:41:38 -0400

    Fixed versioning for development

--- core/library/Version.php
+++ core/library/Version.php
@@ -21,13 +21,13 @@ class ShoppVersion {
 	const MINOR = 3;
 
 	/** @type int PATCH The maintenance patch version number */
-	const PATCH = 9;
+	const PATCH = 4;
 
 	/** @type string PRERELEASE The prerelease designation (dev, beta, RC1) */
-	const PRERELEASE = '';
+	const PRERELEASE = 'dev';
 
 	/** @type string CODENAME The release project code name */
-	const CODENAME = 'Cydonia';
+	const CODENAME = 'Orion';
 
 	/** @type int DB The database schema version */
 	const DB = 1201;
