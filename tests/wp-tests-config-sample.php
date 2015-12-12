7c1e98babd6b3583d4ea5e4fbfe0e6a80ec4c4c5
Jonathan Davis <jond@ingenesis.net>
2015-12-12 12:47:47 -0800

    Add Travis CI to 1.3.x branch

--- tests/wp-tests-config-sample.php
+++ tests/wp-tests-config-sample.php
@@ -1,7 +1,7 @@
 <?php
 
 /* Path to the WordPress codebase you'd like to test. Add a slash to the end. */
-define( 'ABSPATH', '/path/to/wordpress-tests/wordpress/' );
+define( 'ABSPATH', '/tmp/wordpress/src/' );
 
 // Test with multisite enabled: (previously -m)
 // define( 'WP_TESTS_MULTISITE', true );
@@ -21,10 +21,10 @@ define( 'ABSPATH', '/path/to/wordpress-tests/wordpress/' );
 // These tests will DROP ALL TABLES in the database with the prefix named below.
 // DO NOT use a production database or one that is shared with something else.
 
-define( 'DB_NAME', 'shopptest' );    // The name of the database
-define( 'DB_USER', 'shopptest' );     // Your MySQL username
-define( 'DB_PASSWORD', '' ); // ...and password
-define( 'DB_HOST', 'localhost' );    // 99% chance you won't need to change this value
+define( 'DB_NAME', 'testdb' );    // The name of the database
+define( 'DB_USER', 'testuser' );  // Your MySQL username
+define( 'DB_PASSWORD', '' );      // ...and password
+define( 'DB_HOST', 'localhost' ); // 99% chance you won't need to change this value
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
 
