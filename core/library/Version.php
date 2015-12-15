82c1630ff33d99e27d11348f96c02e0601b95989
Jonathan Davis <jond@ingenesis.net>
2015-12-14 21:11:32 -0800

    1.3.10 Release

--- core/library/Version.php
+++ core/library/Version.php
@@ -21,13 +21,13 @@ class ShoppVersion {
 	const MINOR = 3;
 
 	/** @type int PATCH The maintenance patch version number */
-	const PATCH = 9;
+	const PATCH = 10;
 
 	/** @type string PRERELEASE The prerelease designation (dev, beta, RC1) */
 	const PRERELEASE = '';
 
 	/** @type string CODENAME The release project code name */
-	const CODENAME = 'Cydonia';
+	const CODENAME = 'Ares';
 
 	/** @type int DB The database schema version */
 	const DB = 1201;
