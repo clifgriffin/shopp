96d4d5455c2c5a4eff0d7efa7ddb928cc77fabba
Shoppdeveloper.com Team <support@shoppdeveloper.com>
2017-04-25 10:12:13 +0200

    Version adjustment for 1.3.13 release

--- Shopp.php
+++ Shopp.php
@@ -3,11 +3,11 @@
  * Plugin Name: Shopp
  * Plugin URI: http://shopplugin.com
  * Description: An ecommerce framework for WordPress.
- * Version: 1.3.13.dev
+ * Version: 1.3.13
  * Author: Ingenesis Limited
  * Author URI: http://ingenesis.net
  * Requires at least: 3.5
- * Tested up to: 4.6.1
+ * Tested up to: 4.7.4
  *
  *    Portions created by Ingenesis Limited are Copyright © 2008-2014 by Ingenesis Limited
  *
